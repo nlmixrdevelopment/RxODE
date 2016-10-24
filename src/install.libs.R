a141297d2955a807a2dcf458e600dad2fcc1e7dd
Matthew L. Fidler <matthew.fidler@gmail.com>
2016-10-24 11:22:05 -0500

    Add a libRxODE.so to allow linux to work.

---
+++ src/install.libs.R
@@ -0,0 +1,13 @@
+## Copy binaries/libraries to the appropriate directory.
+libs <- file.path(R_PACKAGE_DIR, paste0("libs", R_ARCH));
+files <- file.path(".", sprintf("RxODE%s",.Platform$dynlib.ext));
+message("Installing ", files, " to ", libs);
+file.copy(files, libs, overwrite = TRUE);
+
+if (.Platform$dynlib.ext != ".dll"){
+    libs <- file.path(R_PACKAGE_DIR, paste0("libs", R_ARCH),sprintf("libRxODE%s",.Platform$dynlib.ext));
+    message("Installing ", files, " to ", libs);
+    file.copy(files, libs, overwrite = TRUE);
+}
+
+
