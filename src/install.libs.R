600949bee62d8c30b2690ccdb823fdcbbcefd485
Matthew L. Fidler <matthew.fidler@gmail.com>
2016-10-24 12:27:39 -0500

    Make sure the directory is created

--- src/install.libs.R
+++ src/install.libs.R
@@ -1,5 +1,6 @@
 ## Copy binaries/libraries to the appropriate directory.
 libs <- file.path(R_PACKAGE_DIR, paste0("libs", R_ARCH));
+dir.create(libs,recursive=TRUE);
 files <- file.path(".", sprintf("RxODE%s",.Platform$dynlib.ext));
 message("Installing ", files, " to ", libs);
 file.copy(files, libs, overwrite = TRUE);
