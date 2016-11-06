acc283e7cf9642ebddfaa2e12ccc07662792666b
Fidler <matthew.fidler@gmail.com>
2016-11-05 23:52:52 -0500

    Test all 4 IDR models

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -10,10 +10,11 @@ ode <- "
    d/dt(peri)  =                    Q*C2 - Q*C3;
    d/dt(eff)  = Kin - Kout*(1-C2/(EC50+C2))*eff;
 "
-m1 <- RxODE(model = ode)
+m2 <- RxODE(model = ode)
+tmp <- RxODE:::igraph(m2$cmpMgr$rxDll())
 
 sink("test");
-print(RxODE:::igraph(m1$cmpMgr$rxDll()))
+print(tmp)
 sink()
 tst <- readLines("test");
 unlink("test")
@@ -25,5 +26,62 @@ test_that("igraph works",{
                    "| (e/l), label.font (e/n), label (e/c)",
                    "+ edges (vertex names):",
                    "[1] depot->centr centr->peri  centr->.out1 centr->eff   peri ->centr",
-                   "[6] eff  ->.Kout .Kin ->eff  "),tst)});
+                   "[6] eff  ->.Kout .Kin ->eff  "),tst);
+    expect_true(any(igraph::edge.attributes(tmp)$label == "Indirect\nEffect (II)"));
+});
 
+
+
+ode <- "
+   C2 = centr/V2;
+   C3 = peri/V3;
+   d/dt(depot) =-KA*depot;
+   d/dt(centr) = KA*depot - CL*C2 - Q*C2 + Q*C3;
+   d/dt(peri)  =                    Q*C2 - Q*C3;
+   d/dt(eff)  = Kin*(1-C2/(EC50+C2)) - Kout*eff;
+"
+m1 <- RxODE(model = ode)
+
+tmp <- RxODE:::igraph(m1$cmpMgr$rxDll());
+
+test_that("igraph works",{
+    expect_true(any(igraph::edge.attributes(tmp)$label == "Indirect\nEffect (I)"));
+})
+
+ode <- "
+   C2 = centr/V2;
+   C3 = peri/V3;
+   d/dt(depot) =-KA*depot;
+   d/dt(centr) = KA*depot - CL*C2 - Q*C2 + Q*C3;
+   d/dt(peri)  =                    Q*C2 - Q*C3;
+   d/dt(eff)  = Kin*(1+C2/(EC50+C2)) - Kout*eff;
+"
+
+m3 <- RxODE(model = ode)
+
+tmp <- RxODE:::igraph(m3$cmpMgr$rxDll());
+
+test_that("igraph works",{
+    expect_true(any(igraph::edge.attributes(tmp)$label == "Indirect\nEffect (III)"));
+})
+
+
+ode <- "
+   C2 = centr/V2;
+   C3 = peri/V3;
+   d/dt(depot) =-KA*depot;
+   d/dt(centr) = KA*depot - CL*C2 - Q*C2 + Q*C3;
+   d/dt(peri)  =                    Q*C2 - Q*C3;
+   d/dt(eff)  = Kin - Kout*eff*(1+C2/(EC50+C2));
+"
+
+m4 <- RxODE(model = ode)
+
+tmp <- RxODE:::igraph(m4$cmpMgr$rxDll());
+
+test_that("igraph works",{
+    expect_true(any(igraph::edge.attributes(tmp)$label == "Indirect\nEffect (IV)"));
+})
+
+
+rxClean();
