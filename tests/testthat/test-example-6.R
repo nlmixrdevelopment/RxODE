2a945bdffd28de2930bdbd4bc1c6f9ee6c481e4f
Fidler <matthew.fidler@gmail.com>
2016-11-05 22:46:01 -0500

    Use rxChain and + syntax to add event table

--- tests/testthat/test-example-6.R
+++ tests/testthat/test-example-6.R
@@ -1,5 +1,5 @@
 require(RxODE);
-context("Test Jacobian specification")
+context("Example 6")
 rxClean();
 require(digest)
 ## https://cran.r-project.org/web/packages/diffEq/vignettes/ODEinR.pdf p11
@@ -28,8 +28,23 @@ test_that("infusion model works.",{
     expect_equal(digest(round(as.data.frame(pk),4)),"e76e55f01b5da4f3dadaa23448981b4f")
 })
 
-## Bolus 6.2
+## Change QD dosing to BID dosing.
+et2 <- eventTable(time.units="days")
+et2$add.sampling(seq(0,10,by=1/24))
+et2$add.dosing(dose=2/24,rate=2,strt.time=0,
+               nbr.doses=10,dosing.interval=0.5)
 
+pk2 <- pk + et2;
+
+## plot(pk2$time,pk2$intestine,type="l")
+## plot(pk2$time,pk2$blood,type="l")
+
+test_that("bid infusion works.",{
+    expect_equal(digest(round(as.data.frame(pk2),4)),"e206c7451d6f9939d029f4f1d12b8e88")
+})
+
+
+## Bolus 6.2
 mod <- RxODE("
 b = 0.6
 d/dt(blood) = - b*blood
@@ -39,14 +54,26 @@ et <- eventTable(time.units="days")
 et$add.sampling(seq(0,10,by=1/24));
 et$add.dosing(start.time=0,dose=40,nbr.doses=20,dosing.interval=1);
 
-
 pk <- solve(mod,et)
 
-plot(pk$time,pk$blood,type="l")
+## plot(pk$time,pk$blood,type="l")
 
 test_that("bolus model works",{
     expect_equal(digest(round(as.data.frame(pk),4)),
                  "51d19054249014c5b925dce413cc0f9b")
+})
+
+et2 <- eventTable(time.units="days")
+et2$add.sampling(seq(0,10,by=1/24));
+et2$add.dosing(start.time=0,dose=40,nbr.doses=20,dosing.interval=0.5);
+
+pk2 <- rxChain(pk,et2);
+
+## plot(pk2$time,pk2$blood,type="l")
+
+test_that("bolus model bid works",{
+    expect_equal(digest(round(as.data.frame(pk2),4)),
+                 "43990fa93e861e6194a4014fd53d2c1c")
 })
 
 rxClean()
