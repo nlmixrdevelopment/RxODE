d5cb0f0f348bca63f32d8cdf4b3a91b06ff817e7
Fidler <matthew.fidler@gmail.com>
2016-11-07 08:03:13 -0600

    Add getindex to the testing suite

---
+++ tests/testthat/test-getindex.R
@@ -0,0 +1,27 @@
+require(RxODE)
+context("Get Index");
+
+rxClean();
+rigid.txt <- "
+y1(0)    = 1
+y2(0)    = 0
+y3(0)    = 0.9
+a1       = -2
+a2       = 1.25
+a3       = -0.5
+d/dt(y1) = a1*y2*y3
+d/dt(y2) = a2*y1*y3
+d/dt(y3) = a3*y1*y2
+";
+rigid <- RxODE(rigid.txt)
+
+test_that("Index access the right compartment",{
+    expect_equal(rxState(rigid,"y1") , 1);
+    expect_equal(rxState(rigid,"y2") , 2);
+    expect_equal(rxState(rigid,"y3") , 3);
+    expect_equal(rigid$get.index("y1") , 1);
+    expect_equal(rigid$get.index("y2") , 2);
+    expect_equal(rigid$get.index("y3") , 3);
+})
+
+rxClean();
