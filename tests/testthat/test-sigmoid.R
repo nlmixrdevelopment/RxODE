d1a51025f574eed37cb408aa26bd5adf848b957f
Fidler <matthew.fidler@gmail.com>
2016-11-07 21:54:28 -0600

    Take out spacing between commas for test

--- tests/testthat/test-sigmoid.R
+++ tests/testthat/test-sigmoid.R
@@ -3,9 +3,9 @@ context("Test Plot Parsing")
 require(digest)
 
 test_that("Emax models are detected",{
-    expect_equal(rxSigmoidInfo("Emax*C/(C+E50)"),"Hill(\"Emax\", \"C\", \"E50\", \"1\")");
+    expect_equal(gsub(", +", ",", rxSigmoidInfo("Emax*C/(C+E50)")),"Hill(\"Emax\",\"C\",\"E50\",\"1\")");
 })
 
 test_that("IDR models are detected",{
-    expect_equal(idrInfo("eff","Kin - Kout*(1-C2/(EC50+C2))*eff"), "IDR2(\"Kin\", \"Kout\",  \"EC50\", \"eff\",  \"C2\", edgeList, nodes)")
+    expect_equal(gsub(", +", ",", idrInfo("eff","Kin - Kout*(1-C2/(EC50+C2))*eff")), "IDR2(\"Kin\",\"Kout\",\"EC50\",\"eff\",\"C2\",edgeList,nodes)")
 })
