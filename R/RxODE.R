c584c7f0e9abaa18f6176e9778bdff45a57b03b0
Fidler <matthew.fidler@gmail.com>
2016-11-29 22:24:21 -0600

    Quote "trans" for .Call

--- R/RxODE.R
+++ R/RxODE.R
@@ -1920,7 +1920,7 @@ rxTrans <- function(model,
         }
         parseModel <- tempfile();
         on.exit(unlink(parseModel));
-        ret <- .Call(trans, model, cFile, extraC, modelPrefix, md5, parseModel, PACKAGE="RxODE");
+        ret <- .Call("trans", model, cFile, extraC, modelPrefix, md5, parseModel, PACKAGE="RxODE");
         if (file.exists(cFile)){
             ret$md5 <- c(file_md5 = md5, parsed_md5 = rxMd5(parseModel, extraC)$digest);
             if (modVars){
