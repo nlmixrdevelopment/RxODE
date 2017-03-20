12db0520d14cbf91b39c7453e0b1de76e58b47f2
Fidler <matthew.fidler@gmail.com>
2017-03-20 09:32:23 -0500

    Fix symbolic inverse hash to match the new nubmers in the environment

--- tests/testthat/test-symbolic-inverse.R
+++ tests/testthat/test-symbolic-inverse.R
@@ -13,7 +13,7 @@ rxPermissive({
 
     test_that("Creates the correct matricies,", {
         expect_equal(digest::digest(as.list(symo %>% rxSymInv(1:3))),
-                     "8bdf60e2cb4048314b95158f77727885");
+                     "03b76d0284e9d19eb37bacfdae3294e9");
         expect_equal(symo %>% rxSymInv(1:3, pow=1), structure(c(1, 2, 2, 9), .Dim = c(2L, 2L)));
         expect_equal(symo %>% rxSymInv(1:3, pow=1, 1), structure(c(2, 0, 0, 0), .Dim = c(2L, 2L)));
         expect_equal(symo %>% rxSymInv(1:3, pow=1, 2), structure(c(0, 1, 1, 0), .Dim = c(2L, 2L)));
