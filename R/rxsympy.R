11d3de9cb1bd72528f30ca72fae1b8dd546391e9
Fidler <matthew.fidler@gmail.com>
2017-03-30 14:55:21 -0500

    Some fixes for testhat

--- R/rxsympy.R
+++ R/rxsympy.R
@@ -928,6 +928,7 @@ rxSymPySetupPred <- function(obj, predfn, pkpars=NULL, errfn=NULL, init=NULL, gr
             cat(rf);
             sink();
             sh <- "system"   # windows's default shell COMSPEC does not handle UNC paths
+            Sys.setenv("R_TESTS" = "")
             cmd <- sprintf("%s/bin/Rscript %s",
                            Sys.getenv("R_HOME"), deparse(rfile));
             system(cmd, ignore.stdout=!getOption("RxODE.verbose", TRUE),
