6e30a41038dafb8ee4bb7b83761ef0190fcbfc85
Fidler <matthew.fidler@gmail.com>
2017-04-05 11:51:27 -0500

    Bugfix for cases without gradients

--- R/rxsympy.R
+++ R/rxsympy.R
@@ -1144,9 +1144,9 @@ rxSymPySetupPred <- function(obj, predfn, pkpars=NULL, errfn=NULL, init=NULL, gr
                 rxGc();
                 save(ret, file=cache.file);
                 if (requireNamespace("SnakeCharmR", quietly = TRUE)){
-                    rxCat(sprintf(praise::praise("${Exclamation}! This model has ${created} for FOCEI%s!\nIt will be cached for future runs.\n"), ifelse(grad, "(with Gradient)")))
+                    rxCat(sprintf(praise::praise("${Exclamation}! This model has ${created} for FOCEI%s!\nIt will be cached for future runs.\n"), ifelse(grad, "(with Gradient)", "")))
                 } else {
-                    rxCat(sprintf("Great! This model has created for FOCEI%s!\nIt will be cached for future runs.\n", ifelse(grad, "(with Gradient)")))
+                    rxCat(sprintf("Great! This model has created for FOCEI%s!\nIt will be cached for future runs.\n", ifelse(grad, "(with Gradient)", "")))
                 }
                 if (ret$warn){
                     warning("Some of your prediction function does not depend on the state varibles.");
