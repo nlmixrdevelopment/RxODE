a860b3207d86b1ab9a58c71a0336cb4ebd164953
Matthew Fidler <matthew.fidler@gmail.com>
2017-06-28 21:10:31 -0500

    Export rxReq (for use in nlmixr)

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,10 @@
+##' Require namespace, otherwise throw error.
+##'
+##' @param pkg Package required for function to work.
+##' @return Nothing
+##' @author Matthew L. Fidler
+##' @export
+##' @keywords internal
 rxReq <- function(pkg){
     ## nocov start
     if (!requireNamespace(pkg, quietly = TRUE)) {
