187a86968b5da9f67fac2dd04812631fabec76f4
Matthew Fidler <matthew.fidler@gmail.com>
2017-12-12 21:24:42 -0600

    Add 100+ compartment tests

---
+++ tests/testthat/test-100-cmt.R
@@ -0,0 +1,39 @@
+rxPermissive({
+    context("Use of initial conditions works")
+
+    mod <- RxODE(paste(sprintf("d/dt(amt%s) = -k1*amt%s",1:105,1:105),collapse=";"))
+
+    et <- eventTable() %>% add.sampling(0:180)
+
+    inits <- structure(rep(10, 105), .Names=sprintf("amt%s", 1:105))
+
+    dat <- mod %>% rxSolve(et, c(k1=0.1), inits=inits)
+
+    test_that("initial conditions work", {
+        for (i in 1:104){
+            for (j in seq(i + 1, 105)){
+                expect_equal(dat[, i + 1], dat[, j + 1]);
+            }
+        }
+    })
+
+
+    for (i in 1:105){
+        et$add.dosing(10, dosing.to=i, start.time=0)
+    }
+
+    context("Use of events work")
+
+    dat <- mod %>% rxSolve(et, c(k1=0.1))
+
+    test_that("events work", {
+        for (i in 1:104){
+            for (j in seq(i + 1, 105)){
+                expect_equal(dat[, i + 1], dat[, j + 1]);
+            }
+        }
+    })
+
+})
+
+
