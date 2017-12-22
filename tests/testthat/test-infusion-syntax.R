7bacc82f261d01ac9d832d50b2e4ae13006316fc
Matthew Fidler <matthew.fidler@gmail.com>
2017-12-21 21:42:29 -0600

    Fix infusion syntax tests for new style of C/dll

--- tests/testthat/test-infusion-syntax.R
+++ tests/testthat/test-infusion-syntax.R
@@ -9,7 +9,7 @@ rxPermissive({
     })
 
     test_that("No infusions are in the ODE.", {
-        expect_equal(structure("__DDtStateVar__[0] = _InfusionRate(0) + a*X+Y*Z;\n__DDtStateVar__[1] = _InfusionRate(1) + b*(Y-Z);\n__DDtStateVar__[2] = _InfusionRate(2) + -X*Y+c*Y-Z;\n", .Names = "parseModel"),
+        expect_equal(structure("__DDtStateVar__[0] = _InfusionRate(0, _solveData, _cSub) + a*X+Y*Z;\n__DDtStateVar__[1] = _InfusionRate(1, _solveData, _cSub) + b*(Y-Z);\n__DDtStateVar__[2] = _InfusionRate(2, _solveData, _cSub) + -X*Y+c*Y-Z;\n", .Names = "parseModel"),
                      rxModelVars(rx1)$model["parseModel"])
     })
 
@@ -20,7 +20,7 @@ rxPermissive({
     })
 
     test_that("No infusions are in the ODE.", {
-        expect_equal(structure("__DDtStateVar__[0] = _InfusionRate(0) + a*X+Y*Z+0.0;\n__DDtStateVar__[1] = _InfusionRate(1) + b*(Y-Z)+0.0;\n__DDtStateVar__[2] = _InfusionRate(2) + -X*Y+c*Y-Z+0.0;\n", .Names = "parseModel"),
+        expect_equal(structure("__DDtStateVar__[0] = _InfusionRate(0, _solveData, _cSub) + a*X+Y*Z+0.0;\n__DDtStateVar__[1] = _InfusionRate(1, _solveData, _cSub) + b*(Y-Z)+0.0;\n__DDtStateVar__[2] = _InfusionRate(2, _solveData, _cSub) + -X*Y+c*Y-Z+0.0;\n", .Names = "parseModel"),
                      rxModelVars(rx2)$model["parseModel"])
     })
 
@@ -31,7 +31,7 @@ rxPermissive({
     })
 
     test_that("Can reference prior infusions in current d/dt()", {
-        expect_equal(structure("__DDtStateVar__[0] = _InfusionRate(0) + a*X+Y*Z+0.0;\n__DDtStateVar__[1] = _InfusionRate(1) + b*(Y-Z)+_InfusionRate(0);\n__DDtStateVar__[2] = _InfusionRate(2) + -X*Y+c*Y-Z+_InfusionRate(0);\n", .Names = "parseModel"), rxModelVars(rx3)$model["parseModel"])
+        expect_equal(structure("__DDtStateVar__[0] = _InfusionRate(0, _solveData, _cSub) + a*X+Y*Z+0.0;\n__DDtStateVar__[1] = _InfusionRate(1, _solveData, _cSub) + b*(Y-Z)+_InfusionRate(0, _solveData, _cSub);\n__DDtStateVar__[2] = _InfusionRate(2, _solveData, _cSub) + -X*Y+c*Y-Z+_InfusionRate(0, _solveData, _cSub);\n", .Names = "parseModel"), rxModelVars(rx3)$model["parseModel"])
     })
 
     test_that("rate(Y) before d/dt(Y) throws errors",
