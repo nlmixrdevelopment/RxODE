c9a83cecce7b95c7c4c2f8534a0cbd84999e061f
Fidler <matthew.fidler@gmail.com>
2018-03-20 00:04:48 -0500

    Revert "Add covariate check for each method"
    This reverts commit b45e2128210ac0d2880d8176a29368ecea384e1c.

--- tests/testthat/test-cov.R
+++ tests/testthat/test-cov.R
@@ -1,192 +1,190 @@
 library(RxODE)
 rxPermissive({
     rxClean()
-    for (m in c("liblsoda", "lsoda", "dop853")){
-        context(sprintf("Simple test for time-varying covariates (%s)", method=m))
 
-        ode <- RxODE({
-            b       = -1
-            d/dt(X) = a*X + Y*Z;
-            d/dt(Y) = b*(Y - Z);
-            d/dt(Z) = -X*Y + c*Y - Z
-            printf("%.10f,%.10f\n",t,c);
-        })
+    context("Simple test for time-varying covariates")
 
-        et <- eventTable()   # default time units
-        et$add.sampling(seq(from=0, to=100, by=0.01))
+    ode <- RxODE({
+        b       = -1
+        d/dt(X) = a*X + Y*Z;
+        d/dt(Y) = b*(Y - Z);
+        d/dt(Z) = -X*Y + c*Y - Z
+        printf("%.10f,%.10f\n",t,c);
+    })
 
-        cov <- data.frame(c=et$get.sampling()$time+1);
+    et <- eventTable()   # default time units
+    et$add.sampling(seq(from=0, to=100, by=0.01))
 
-        cov.lin <- approxfun(et$get.sampling()$time, cov$c, yleft=cov$c[1], yright=cov$c[length(cov$c)]);
+    cov <- data.frame(c=et$get.sampling()$time+1);
 
-        sink("temp.csv");
-        cat("t,c\n");
-        out <- rxSolve(ode,
-                       params = c(a=-8/3, b=-10),
-                       events = et,
-                       inits = c(X=1, Y=1, Z=1),
-                       covs = cov, add.cov=TRUE, method=m);
-        sink();
-        lin.interp <- read.csv("temp.csv");
-        unlink("temp.csv");
+    cov.lin <- approxfun(et$get.sampling()$time, cov$c, yleft=cov$c[1], yright=cov$c[length(cov$c)]);
 
-        lin.interp$c2 <- cov.lin(lin.interp$t);
+    sink("temp.csv");
+    cat("t,c\n");
+    out <- rxSolve(ode,
+                   params = c(a=-8/3, b=-10),
+                   events = et,
+                   inits = c(X=1, Y=1, Z=1),
+                   covs = cov, add.cov=TRUE);
+    sink();
+    lin.interp <- read.csv("temp.csv");
+    unlink("temp.csv");
 
-        test_that("time varying covariates output covariate in data frame",{
-            expect_equal(cov$c,out$c);
-        })
+    lin.interp$c2 <- cov.lin(lin.interp$t);
 
-        test_that("Linear Approximation matches approxfun.", {
-            expect_equal(lin.interp$c, lin.interp$c2);
-        })
+    test_that("time varying covariates output covariate in data frame",{
+        expect_equal(cov$c,out$c);
+    })
 
-        ## NONMEM interpolation
-        sink("temp.csv");
-        cat("t,c\n");
-        out <- rxSolve(ode,
-                       params = c(a=-8/3, b=-10),
-                       events = et,
-                       inits = c(X=1, Y=1, Z=1),
-                       covs = cov,
-                       covs_interpolation="NOCB", add.cov=TRUE, method=m);
-        sink()
-        lin.interp <- read.csv("temp.csv");
-        unlink("temp.csv");
+    test_that("Linear Approximation matches approxfun.", {
+        expect_equal(lin.interp$c, lin.interp$c2);
+    })
 
-        cov.lin <- approxfun(out$time, out$c, yleft=cov$c[1], yright=cov$c[length(cov$c)],
-                             method="constant", f=1);
-        lin.interp$c2 <- cov.lin(lin.interp$t);
+    ## NONMEM interpolation
+    sink("temp.csv");
+    cat("t,c\n");
+    out <- rxSolve(ode,
+                   params = c(a=-8/3, b=-10),
+                   events = et,
+                   inits = c(X=1, Y=1, Z=1),
+                   covs = cov,
+                   covs_interpolation="NOCB", add.cov=TRUE);
+    sink()
+    lin.interp <- read.csv("temp.csv");
+    unlink("temp.csv");
 
-        test_that("NOCB Approximation similar to approxfun.", {
-            expect_equal(lin.interp$c, lin.interp$c2);
-        })
+    cov.lin <- approxfun(out$time, out$c, yleft=cov$c[1], yright=cov$c[length(cov$c)],
+                         method="constant", f=1);
+    lin.interp$c2 <- cov.lin(lin.interp$t);
 
-
-        ## midpoint interpolation
-        sink("temp.csv");
-        cat("t,c\n");
-        out <- rxSolve(ode,
-                       params = c(a=-8/3, b=-10),
-                       events = et,
-                       inits = c(X=1, Y=1, Z=1),
-                       covs = cov,
-                       covs_interpolation="midpoint", add.cov=TRUE, method=m);
-        sink()
-        lin.interp <- read.csv("temp.csv");
-        unlink("temp.csv");
-
-        cov.lin <- approxfun(out$time, out$c, yleft=cov$c[1], yright=cov$c[length(cov$c)],
-                             method="constant", f=0.5);
-
-        lin.interp$c2 <- cov.lin(lin.interp$t);
-
-        test_that("midpoint Approximation similar to approxfun.", {
-            expect_equal(lin.interp$c, lin.interp$c2);
-        })
+    test_that("NOCB Approximation similar to approxfun.", {
+        expect_equal(lin.interp$c, lin.interp$c2);
+    })
 
 
-        ## covs_interpolation
-        sink("temp.csv");
-        cat("t,c\n");
-        out <- rxSolve(ode,
-                       params = c(a=-8/3, b=-10),
-                       events = et,
-                       inits = c(X=1, Y=1, Z=1),
-                       covs = cov,
-                       covs_interpolation="constant", add.cov=TRUE, method=m);
-        sink()
-        lin.interp <- read.csv("temp.csv");
-        unlink("temp.csv");
+    ## midpoint interpolation
+    sink("temp.csv");
+    cat("t,c\n");
+    out <- rxSolve(ode,
+                   params = c(a=-8/3, b=-10),
+                   events = et,
+                   inits = c(X=1, Y=1, Z=1),
+                   covs = cov,
+                   covs_interpolation="midpoint", add.cov=TRUE);
+    sink()
+    lin.interp <- read.csv("temp.csv");
+    unlink("temp.csv");
 
-        cov.lin <- approxfun(out$time, out$c, yleft=cov$c[1], yright=cov$c[length(cov$c)],
-                             method="constant");
+    cov.lin <- approxfun(out$time, out$c, yleft=cov$c[1], yright=cov$c[length(cov$c)],
+                         method="constant", f=0.5);
 
-        lin.interp$c2 <- cov.lin(lin.interp$t);
+    lin.interp$c2 <- cov.lin(lin.interp$t);
 
-        test_that("Constant Approximation similar to approxfun.", {
-            expect_equal(lin.interp$c, lin.interp$c2);
-        })
+    test_that("midpoint Approximation similar to approxfun.", {
+        expect_equal(lin.interp$c, lin.interp$c2);
+    })
 
 
-        out <- as.data.frame(out);
-        out <- out[,names(out) != "c"];
+    ## covs_interpolation
+    sink("temp.csv");
+    cat("t,c\n");
+    out <- rxSolve(ode,
+                   params = c(a=-8/3, b=-10),
+                   events = et,
+                   inits = c(X=1, Y=1, Z=1),
+                   covs = cov,
+                   covs_interpolation="constant", add.cov=TRUE);
+    sink()
+    lin.interp <- read.csv("temp.csv");
+    unlink("temp.csv");
 
-        sink("temp");
-        out1 <-
-            rxSolve(ode,
-                    params = c(a=-8/3, b=-10, c = 0),
+    cov.lin <- approxfun(out$time, out$c, yleft=cov$c[1], yright=cov$c[length(cov$c)],
+                         method="constant");
+
+    lin.interp$c2 <- cov.lin(lin.interp$t);
+
+    test_that("Constant Approximation similar to approxfun.", {
+        expect_equal(lin.interp$c, lin.interp$c2);
+    })
+
+
+    out <- as.data.frame(out);
+    out <- out[,names(out) != "c"];
+
+    sink("temp");
+    out1 <-
+        rxSolve(ode,
+                params = c(a=-8/3, b=-10, c = 0),
+                events = et,
+                inits = c(X=1, Y=1, Z=1), add.cov=TRUE)
+    sink();
+    unlink("temp");
+
+    out1 <- as.data.frame(out1);
+
+    test_that("time varying covariates produce different outputs",{
+        expect_false(isTRUE(all.equal(out,out1)));
+    })
+
+    cov <- data.frame(c=et$get.sampling()$time+1,a=-et$get.sampling()$time/100);
+
+    sink("temp")
+    out <- rxSolve(ode,
+                   params = c(a=-8/3, b=-10),
+                   events = et,
+                   inits = c(X=1, Y=1, Z=1),
+                   covs = cov, add.cov=TRUE)
+
+    out3 <- rxSolve(ode,
+                    params = c(a=-8/3, b=-10),
                     events = et,
-                    inits = c(X=1, Y=1, Z=1), add.cov=TRUE, method=m)
-        sink();
-        unlink("temp");
+                    inits = c(X=1, Y=1, Z=1),
+                    covs = cov, add.cov=TRUE)
+    sink();
+    unlink("temp");
 
-        out1 <- as.data.frame(out1);
-
-        test_that("time varying covariates produce different outputs",{
-            expect_false(isTRUE(all.equal(out,out1)));
-        })
-
-        cov <- data.frame(c=et$get.sampling()$time+1,a=-et$get.sampling()$time/100);
-
-        sink("temp")
-        out <- rxSolve(ode,
-                       params = c(a=-8/3, b=-10),
-                       events = et,
-                       inits = c(X=1, Y=1, Z=1),
-                       covs = cov, add.cov=TRUE, method=m)
-
-        out3 <- rxSolve(ode,
-                        params = c(a=-8/3, b=-10),
-                        events = et,
-                        inits = c(X=1, Y=1, Z=1),
-                        covs = cov, add.cov=TRUE, method=m)
-        sink();
-        unlink("temp");
-
-        test_that("time varying covariates output covariate in data frame",{
-            expect_equal(cov$c,out$c);
-            expect_equal(cov$a,out$a);
-        })
+    test_that("time varying covariates output covariate in data frame",{
+        expect_equal(cov$c,out$c);
+        expect_equal(cov$a,out$a);
+    })
 
 
-        cov <- data.frame(c=et$get.sampling()$time+1);
+    cov <- data.frame(c=et$get.sampling()$time+1);
 
-        sink("temp");
-        out2 <- rxSolve(ode,
-                        params = c(a=-8/3, b=-10),
-                        events = et,
-                        inits = c(X=1, Y=1, Z=1),
-                        covs = cov, add.cov=TRUE, method=m)
-        sink();
-        unlink("temp");
+    sink("temp");
+    out2 <- rxSolve(ode,
+                    params = c(a=-8/3, b=-10),
+                    events = et,
+                    inits = c(X=1, Y=1, Z=1),
+                    covs = cov, add.cov=TRUE)
+    sink();
+    unlink("temp");
 
-        test_that("Before assinging the time varying to -8/3, out and out2 should be different",{
-            expect_false(isTRUE(all.equal(out,out2)));
-        })
+    test_that("Before assinging the time varying to -8/3, out and out2 should be different",{
+        expect_false(isTRUE(all.equal(out,out2)));
+    })
 
-        context(sprintf("Test First Assignment (%s)", method=m))
-        ## Assign a time-varying to a simple parameter
-        sink("temp");
-        out$a <- -8/3
-        sink();
-        unlink("temp");
+    context("Test First Assignment")
+    ## Assign a time-varying to a simple parameter
+    sink("temp");
+    out$a <- -8/3
+    sink();
+    unlink("temp");
 
-        test_that("The out$a=-8/3 works.",{
-            expect_equal(as.data.frame(out),as.data.frame(out2));
-        })
+    test_that("The out$a=-8/3 works.",{
+        expect_equal(as.data.frame(out),as.data.frame(out2));
+    })
 
 
-        context(sprintf("Test Second Assignment (%s)", method=m))
-        sink("temp")
-        out$a <- out3$a
-        sink();
-        unlink("temp");
+    context("Test Second Assignment")
+    sink("temp")
+    out$a <- out3$a
+    sink();
+    unlink("temp");
 
-        test_that("the out$a = time varying covariate works.",{
-            expect_equal(as.data.frame(out),as.data.frame(out3));
-        })
-    }
-
+    test_that("the out$a = time varying covariate works.",{
+        expect_equal(as.data.frame(out),as.data.frame(out3));
+    })
 
     rxClean()
 }, silent=TRUE, cran=TRUE)
