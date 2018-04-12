5e59abbcb5ecd39eb0228cc12c0235041c2b2422
Fidler <matthew.fidler@gmail.com>
2018-04-12 13:05:16 -0500

    Fix rxShiny to work without shiny

--- R/rxShiny.R
+++ R/rxShiny.R
@@ -1,63 +1,73 @@
-##' Run Shiny app on RxODE object
+##' Use Shiny to help develop an RxODE model
 ##'
-##' @param object  RxODE family of objects.
-##' @param ... Other arguments passed to rxShiny
+##' @param object A RxODE family of objects. If not supplied a
+##'     2-compartment indirect effect model is used.  If it is
+##'     supplied, use the model associated with the RxODE object for
+##'     the model exploration.
+##' @param ... Other arguments passed to rxShiny.  Currently doesn't
+##'     do anything.
 ##' @return Nothing; Starts a shiny server
 ##' @author Zufar Mulyukov and Matthew L. Fidler
 rxShiny <- function(object, ...){
     UseMethod("rxShiny");
 }
-
 ##' @rdname rxShiny
-rxShiny.rxSolve <- function(object, ...){
-    ui<- eval(bquote(shinyUI(fluidPage(
-        tags$style(HTML("input:invalid {background-color: #FFCCCC;}")),
-        tags$script('
+rxShiny.default <- function(object="
+C2 = centr/V2
+C3 = peri/V3
+d/dt(depot) =-KA*depot
+d/dt(centr) = KA*depot - CL*C2 - Q*C2 + Q*C3
+d/dt(peri)  =                    Q*C2 - Q*C3
+d/dt(eff)  = Kin - Kout*(1-C2/(EC50+C2))*eff
+", ...){
+    ui<- eval(bquote(shiny::shinyUI(shiny::fluidPage(
+                                               shiny::tags$style(shiny::HTML("input:invalid {background-color: #FFCCCC;}")),
+        shiny::tags$script('
 $(document).on("keyup", function(e) {
   if(e.keyCode == 13){
-  Shiny.onInputChange("EnterPressed", Math.random());
+  shiny::Shiny.onInputChange("EnterPressed", Math.random());
   }});
   '
 ),
-fluidRow(
-    column(12,
-           fluidRow(
-               column(5,
-                      textAreaInput(
+shiny::fluidRow(
+    shiny::column(12,
+           shiny::fluidRow(
+               shiny::column(5,
+                      shiny::textAreaInput(
                           inputId = "ode",
                           label = "ODE",
                           width = 360,
                           height = 200,
                           resize = 'both',
-                          value = .(rxNorm(object))),
+                          value=.(rxNorm(object))),
 
-actionButton("goButton", "Compile", align='right'),
-hr(),
-h4('Time sampling'),
-tags$div(style="display: inline-block;",textInput('tmin', 'start', width=60, value = 0)),
-tags$div(style="display: inline-block;",textInput('tmax', 'end'  , width=60, value = 100)),
-tags$div(style="display: inline-block;",textInput('step', 'step' , width=60, value = 1)),
-hr(),
-h4('Dosing'),
-tags$div(style="display: inline-block;",textInput('dose', 'amount', width=60, value=1)),
-tags$div(style="display: inline-block;",textInput('rate', 'rate'  , width=60)),
-div(style="display: inline-block;",uiOutput("dosing_cmt")),
-br(),
-tags$div(style="display: inline-block;",textInput('start'   , 'start'    , width=60, value = 0)),
-tags$div(style="display: inline-block;",textInput('ndoses' ,  'repeat'   , width=60, value = 1)),
-tags$div(style="display: inline-block;",textInput('interval', 'interval' , width=60, value = 0))
+shiny::actionButton("goButton", "Compile", align='right'),
+shiny::hr(),
+shiny::h4('Time sampling'),
+shiny::tags$div(style="display: inline-block;",shiny::textInput('tmin', 'start', width=60, value = 0)),
+shiny::tags$div(style="display: inline-block;",shiny::textInput('tmax', 'end'  , width=60, value = 100)),
+shiny::tags$div(style="display: inline-block;",shiny::textInput('step', 'step' , width=60, value = 1)),
+shiny::hr(),
+shiny::h4('Dosing'),
+shiny::tags$div(style="display: inline-block;",shiny::textInput('dose', 'amount', width=60, value=1)),
+shiny::tags$div(style="display: inline-block;",shiny::textInput('rate', 'rate'  , width=60)),
+shiny::div(style="display: inline-block;",shiny::uiOutput("dosing_cmt")),
+shiny::br(),
+shiny::tags$div(style="display: inline-block;",shiny::textInput('start'   , 'start'    , width=60, value = 0)),
+shiny::tags$div(style="display: inline-block;",shiny::textInput('ndoses' ,  'repeat'   , width=60, value = 1)),
+shiny::tags$div(style="display: inline-block;",shiny::textInput('interval', 'interval' , width=60, value = 0))
 
 ),
-column(width = 7,
-       fluidRow(
-           h4('Initial values'),
-           uiOutput("cmts"),
-           br(),
-           h4('Parameters'),
-           uiOutput("pars"),
-           uiOutput('message', placeholder = FALSE),
-           ## So may parens;  looks like lisp :)
-           uiOutput("plotTabs"))))))))))
+shiny::column(width = 7,
+       shiny::fluidRow(
+           shiny::h4('Initial values'),
+           shiny::uiOutput("cmts"),
+           shiny::br(),
+           shiny::h4('Parameters'),
+           shiny::uiOutput("pars"),
+           shiny::uiOutput('message', placeholder = FALSE),
+           shiny::uiOutput("plotTabs")
+       )))))))))
 
     server <- function(input, output, session) {
         values <- reactiveValues()
@@ -68,7 +78,7 @@ column(width = 7,
 
         tmp=tempfile()
 
-        observeEvent(input$goButton, {
+        shiny::observeEvent(input$goButton, {
             values$m1=NULL
             values$res <- NULL
 
@@ -77,15 +87,16 @@ column(width = 7,
 
                     values$m1<-RxODE(model = input$ode, wd = tmp)
                     values$cmts = values$m1$get.modelVars()$state
-                    values$pars = values$m1$get.modelVars()$params
+                    values$pars = rxParams(values$m1)
 
                 }, error = function(e) return(e))
             )
 
+
         })
         observe({if(!is.null(values$m1)) values$msg = 'Model is compiled. Enter parameters.'})
 
-        output$message <- renderPrint({
+        output$message <- shiny::renderPrint({
 
             if(length(values$msg)==0)
                 return(invisible())
@@ -96,35 +107,35 @@ column(width = 7,
             cat(paste(gsub('<|>','',values$msg), collapse = '<br>'))
         })
 
-        output$dosing_cmt <- renderUI({
+        output$dosing_cmt <- shiny::renderUI({
             if (is.null(values$cmts))
                 return()
             selectInput("into", "into", width=80, choices = values$cmts, selected=values$cmts[1])
         })
 
-        output$cmts <- renderUI({
+        output$cmts <- shiny::renderUI({
             if (is.null(values$cmts))
                 return()
             if(length(values$cmts)==0)
                 return('none')
 
             lapply(values$cmts, function(x) {
-                div(style="display: inline-block;", textInput(x, x, value = 0, width=60))
+                shiny::div(style="display: inline-block;", shiny::textInput(x, x, value = 0, width=60))
             })
         })
 
-        output$pars <- renderUI({
+        output$pars <- shiny::renderUI({
             if (is.null(values$pars))
                 return()
             if(length(values$pars)==0)
                 return('none')
 
             lapply(values$pars, function(x) {
-                div(style="display: inline-block;", textInput(x, x, value = 1, width=60))
+                shiny::div(style="display: inline-block;", shiny::textInput(x, x, value = 1, width=60))
             })
         })
 
-        output$plotTabs = renderUI({
+        output$plotTabs = shiny::renderUI({
             if (is.null(values$res))
                 return(tabsetPanel())
             dat=values$res
@@ -137,15 +148,13 @@ column(width = 7,
                 lapply(cmts, function(cmt){
                     plotname <- paste("plot", cmt, sep="")
                     output[[plotname]] <- renderPlot({
-                        ggplot(as.data.frame(dat), aes_(x=as.name("time"), y=as.name(cmt))) + geom_line()
+                        ggplot2::ggplot(as.data.frame(dat), ggplot2::aes_(x=as.name("time"), y=as.name(cmt))) + ggplot2::geom_line()
                     })
 
                     tabPanel(cmt,  plotOutput(plotname))
                 })
             do.call(tabsetPanel, c(tabs, id='plot.tabs', selected = sel.tab))
         })
-
-
 
         observeEvent({input$EnterPressed},{
             values$msg <- capture.output(
@@ -185,7 +194,10 @@ column(width = 7,
                           dosing.interval = interval,
                           dosing.to = into)
 
-            params <- .(object$params.single)
+            params <-
+                c(KA = .291, CL = 18.6,
+                  V2 = 40.2, Q = 10.5, V3 = 297.0,
+                  Kin = 1.0, Kout = 1.0, EC50 = 200.0)
 
             cmts = values$cmts
 
@@ -212,7 +224,5 @@ column(width = 7,
     }
 
 
-    shinyApp(ui = ui, server = server)
+    shiny::shinyApp(ui = ui, server = server)
 }
-
-
