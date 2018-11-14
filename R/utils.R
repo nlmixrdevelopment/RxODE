4d119b128522a5d50ba2737df269bfd8f86e1602
Fidler <matthew.fidler@gmail.com>
2018-11-14 00:42:31 -0600

    Fix Issue #56 and #57

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,8 @@
-.normalizePath <- function(...){
+.normalizePath <- function(path, ...){
     ifelse(.Platform$OS.type=="windows",
-           suppressWarnings(utils::shortPathName(normalizePath(...))),
-    ifelse(regexpr("^/") != -1, paste(...),
-           suppressWarnings(normalizePath(...))))
+           suppressWarnings(utils::shortPathName(normalizePath(path, ...))),
+    ifelse(regexpr("^/", path) != -1, path,
+           suppressWarnings(normalizePath(path, ...))))
 }
 
 ##' Require namespace, otherwise throw error.
