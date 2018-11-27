363460eb3b3bf4eee6599df337a9a75913ff987b
Matthew L. Fidler <514778+mattfidler@users.noreply.github.com>
2018-11-27 11:27:35 -0600

    Add Test for Issue #56

---
+++ tests/testthat/test-issue-56.R
@@ -0,0 +1,13 @@
+rxPermissive({
+    context("Absolute Working Directory + Model Name (Issue #56)")
+    test_that("Issue #56", {
+        if (!dir.exists("/tmp/")){
+            skip()
+        }
+        ode <- " d/dt(test) = 0; "
+        m1 <- RxODE(model = ode, modName = "m1", wd = "/tmp")
+        expect_true(dir.exists("/tmp/m1.d"))
+        rxDelete(m1);
+        unlink("/tmp/m1.d", recursive=TRUE)
+    })
+}, silent=TRUE, cran=TRUE)
