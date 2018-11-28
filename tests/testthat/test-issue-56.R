3f4ebc598d274bb070ea2405b00b846b5b95a1e4
Matthew L. Fidler <514778+mattfidler@users.noreply.github.com>
2018-11-27 22:56:12 -0600

    Skip test 56 on Windows

--- tests/testthat/test-issue-56.R
+++ tests/testthat/test-issue-56.R
@@ -1,6 +1,7 @@
 rxPermissive({
     context("Absolute Working Directory + Model Name (Issue #56)")
     test_that("Issue #56", {
+        skip_on_os("windows")
         if (!dir.exists("/tmp/")){
             skip()
         }
