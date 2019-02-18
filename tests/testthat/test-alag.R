17cd39869746257f3c866a46984fd32be2c797e7
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2019-02-18 16:53:48 -0600

    Add more info to alag test

--- tests/testthat/test-alag.R
+++ tests/testthat/test-alag.R
@@ -11,10 +11,13 @@ rxPermissive({
         d/dt(blood)     = a*intestine - b*blood
     })
 
+    obs <- units::set_units(seq(0,10,by=1/24),"days");
+
     et <- eventTable(time.units="days")
-    et$add.sampling(seq(0,10,by=1/24))
+    et$add.sampling(obs)
     et$add.dosing(dose=2/24,start.time=0,
                   nbr.doses=10,dosing.interval=1)
+
 
     solve1 <- solve(mod,et)
 
@@ -29,6 +32,8 @@ rxPermissive({
     solve2 <- solve(mod2,et)
 
     test_that("Solves with lag times are different", {
+        expect_equal(obs,solve1$time)
+        expect_equal(obs,solve2$time)
         expect_false(all(solve1$intestine ==solve2$intestine))
         expect_false(all(solve1$blood ==solve2$blood))
     })
@@ -41,6 +46,7 @@ rxPermissive({
     solve3 <- solve(mod, et)
 
     test_that("Absorption lag shifts event by 2", {
+        expect_equal(obs,solve3$time)
         expect_equal(solve3$intestine, solve2$intestine)
         expect_equal(solve3$blood, solve2$blood)
     })
