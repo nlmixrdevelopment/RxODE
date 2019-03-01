069615ba6c311d0ed24eb77dffc5bdf999b4f6df
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2019-03-01 14:32:33 -0600

    Zero-variable fix

--- R/RxODE.R
+++ R/RxODE.R
@@ -1252,8 +1252,10 @@ rxCompile.rxModelVars <-  function(model, # Model
                               }), collapse=" "))
             } else {
                 .fixInis <- c(sprintf("double _theta[%d];",length(.mv$params)),
-                              paste(paste0("_theta[",seq_along(.mv$params)-1,"] = theta[",
-                                           seq_along(.mv$params)-1,"];"),collapse="\n"));
+                              ifelse(length(.mv$params)==0,
+                                     "",
+                                     paste(paste0("_theta[",seq_along(.mv$params)-1,"] = theta[",
+                                           seq_along(.mv$params)-1,"];"),collapse="\n")));
             }
             .trans <- c(.mv$trans, .mv$md5);
             .trans["fix_inis"] <- .fixInis[2];
