5b11945962fbe1aa4d1a279df684ea746a71b3ac
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2019-07-30 15:18:45 -0500

    Take out python setup from windows environment

--- R/RxODE-win-setup.R
+++ R/RxODE-win-setup.R
@@ -59,40 +59,6 @@ rxPhysicalDrives <- memoise::memoise(function(duplicates=FALSE){
         return(ret)
     }
 })
-.rxPythonBaseWin <- function(){
-    if (.Platform$OS.type == "unix"){
-    } else if (file.exists(R.home("rxPython"))){
-        R.home("rxPython")
-    } else {
-        .keys <- .rxNlmixr();
-        if (!is.null(.keys$pythonBase)){
-            .pythonBase <- .keys$pythonBase;
-        } else {
-            .keys <- NULL
-            .keys <- try(utils::readRegistry("SOFTWARE\\Python\\PythonCore", hive = "HCU", ## view = "32-bit",
-                                            maxdepth = 3), silent=TRUE);
-            if (is.null(.keys) || length(.keys) == 0 || inherits(.keys, "try-error")){
-                try(.keys <- utils::readRegistry("SOFTWARE\\Python\\PythonCore", hive = "HLM", ## view = "32-bit",
-                                                maxdepth = 3), silent = TRUE);
-            }
-            .pythonBase <- NULL
-            for (i in seq_along(.keys)){
-                try(.pythonBase <- .keys[[i]]$InstallPath$`(Default)`, silent=TRUE)
-                if (!is.null(.pythonBase)){
-                    if (file.exists(file.path(.pythonBase, "python.exe"))){
-                        break;
-                    } else {
-                        .pythonBase <- NULL;
-                    }
-                }
-            }
-        }
-        if (!is.null(.pythonBase)){
-            .pythonBase <- gsub("\\", "/", .normalizePath(gsub(rex::rex(any_of("/", "\\"), end), "", .pythonBase)), fixed=TRUE);
-        }
-        return(.pythonBase)
-    }
-}
 
 .rxNlmixr <- memoise::memoise(function(){
     .keys <- NULL
@@ -108,10 +74,8 @@ rxPhysicalDrives <- memoise::memoise(function(duplicates=FALSE){
             }
         }
         .lst <- list(
-            rtoolsBase = .normalizePath(file.path(.keys[[1]], "rtools"), winslash="/", mustWork=FALSE),
-            pythonBase=.normalizePath(file.path(.keys[[1]], "python"), winslash="/", mustWork=FALSE)
-        );
-        if (dir.exists(.lst$rtoolsBase) && dir.exists(.lst$pythonBase)){
+            rtoolsBase = .normalizePath(file.path(.keys[[1]], "rtools"), winslash="/", mustWork=FALSE));
+        if (dir.exists(.lst$rtoolsBase)){
             return(.lst);
         }
     }
@@ -213,13 +177,11 @@ rxPhysicalDrives <- memoise::memoise(function(duplicates=FALSE){
 ##'
 ##' @param rm.rtools Remove the Rtools from the current path specs.
 ##'
-##' @param rm.python Remove Python from the current path specs.
-##'
 ##' @param retry Should you retry to find Rtools?  If NA, don't throw
 ##'     an error if it isn't found.
 ##'
 ##' @author Matthew L. Fidler
-.rxWinRtoolsPath <- function(rm.rtools=TRUE, rm.python=TRUE, retry=FALSE){
+.rxWinRtoolsPath <- function(rm.rtools=TRUE, retry=FALSE){
     ## Note that devtools seems to assume that rtools/bin is setup
     ## appropriately, and figures out the c compiler from there.
     if (.Platform$OS.type == "unix"){
@@ -236,12 +198,8 @@ rxPhysicalDrives <- memoise::memoise(function(duplicates=FALSE){
         }))
         .path <- .path[.path != ""];
         if (!inherits(rm.rtools, "logical")) rm.rtools <- FALSE
-        if (!inherits(rm.python, "logical")) rm.python <- FALSE
         if (rm.rtools){
             .path <- .path[regexpr(rex::rex(or("Rtools", "RTOOLS", "rtools")), .path) == -1]
-        }
-        if (rm.python){
-            .path <- .path[regexpr(rex::rex(or("Python", "python", "PYTHON")), .path) == -1]
         }
         .rPath <- .normalizePath(file.path(Sys.getenv("R_HOME"), paste0("bin", Sys.getenv("R_ARCH"))));
         .path <- c(.rPath, .path);
@@ -276,31 +234,6 @@ rxPhysicalDrives <- memoise::memoise(function(duplicates=FALSE){
                     .path <- c(.normalizePath(.x), .path);
                 }
             }
-            .pythonBase <- .rxPythonBaseWin();
-            if (!is.null(.pythonBase)){
-                python <- .normalizePath(file.path(.pythonBase, "python.exe"));
-                if (file.exists(python)){
-                    ## Sometimes there are 2 competing python
-                    ## installs.  Make sure to setup everything, just
-                    ## in case... Otherwise it can crash R :(
-                    Sys.setenv(PYTHON_EXE=.normalizePath(python)); ## For PythonInR
-                    .path <- c(.normalizePath(.pythonBase), .path);
-                    Sys.setenv(PYTHONHOME=.normalizePath(.pythonBase));
-                    Sys.setenv(PYTHON_INCLUDE=.normalizePath(file.path(.pythonBase, "include")));
-                    .pythonLibBase <- .normalizePath(file.path(.pythonBase, "libs"))
-                    .pythonLibName <- list.files(.pythonLibBase, "[0-9][0-9]+\\.lib$");
-                    if (length(.pythonLibName) == 1){
-                        Sys.setenv(PYTHON_LIB=.normalizePath(file.path(.pythonBase, "libs", .pythonLibName)));
-                    } else {
-                        Sys.unsetenv("PYTHON_LIB")
-                    }
-                    Sys.setenv(PYTHONPATH=paste(c(.normalizePath(file.path(.pythonBase, "DLLs")),
-                                                  .normalizePath(file.path(.pythonBase, "Lib")),
-                                                  .normalizePath(file.path(.pythonBase, "Lib", "site-packages"))),
-                                                collapse=";"));
-                    Sys.unsetenv("PYTHONSTARTUP");
-                }
-            }
             ## java <- as.vector(Sys.which("java"));
             ## if (java != ""){
             ##     java <- sub(rex::rex(one_of("/", "\\"), except_any_of("/", "\\", "\n"), end), "", java)
@@ -333,62 +266,13 @@ rxPhysicalDrives <- memoise::memoise(function(duplicates=FALSE){
         }
     }
 }
-.installRxPython <- function(dir=R.home("rxPython"),
-                             opt="InstallAllUsers=0 AssociateFiles=0 Shortcuts=0"){
-    dir <- .normalizePath(dir);
-    opt <- sprintf("%s DefaultJustForMeTargetDir=%s", opt, dir, dir)
-    .x64 <- (regexpr("x64", Sys.info()["release"]) != -1)
-    ## if (x64) opt <- sprintf("/passive %s", opt)
-    .tmp <- try({installr::install.python(installer_option=opt, x64=.x64)}, silent=TRUE)
-    if (inherits(.tmp, "try-error")){
-        ## Installr is a bit old in MRAN support that possibility
-        installr::install.python(installer_option=opt)
-    }
-    system(sprintf("%s/python -m pip install --upgrade pip", R.home("rxPython")))
-    system(sprintf("%s/python -m pip install --upgrade sympy", R.home("rxPython")))
-    system(sprintf("%s/python -m pip install --upgrade numpy", R.home("rxPython")))
-}
-##' Setup Python and SymPy for windows
-##'
-##' @author Matthew L. Fidler
-##' @export
-rxWinPythonSetup <- function(){
-    .base <- .rxPythonBaseWin()
-    if (is.null(.base)){
-        .installRxPython()
-        .base <- .rxPythonBaseWin()
-        if (is.null(.base)){
-            stop("RxODE requires Python. Please install an appropriate version and add it to the system path.")
-        }
-    }
-    if (file.access(paste(.base, "/Lib/site-packages", sep=""),2)==-1){
-      stop("The Python library path does not appear to be writeable. Please rectify this situation, restart R, and try again.")
-    }
-    .tmp <- try({rxSymPyVersion()})
-    if (inherits(.tmp, "try-error")){
-        system(sprintf("%s/python -m pip install --upgrade pip", .rxPythonBaseWin()))
-        system(sprintf("%s/python -m pip install --upgrade sympy", .rxPythonBaseWin()))
-        system(sprintf("%s/python -m pip install --upgrade numpy", .rxPythonBaseWin()))
-    }
-    utils::install.packages("reticulate");
-    .tmp <- try({rxSymPyVersion()});
-    if (inherits(.tmp, "try-error")){
-        remotes::install_github("nlmixrdevelopment/SnakeCharmR")
-        .tmp <- try({rxSymPyVersion()});
-        if (inherits(.tmp, "try-errror")){
-            stop("Cannot setup RxODE<->sympy link");
-        }
-    }
-}
-
 ##' Setup Windows components for RxODE
 ##'
 ##' @inheritParams .rxWinRtoolsPath
 ##' @author Matthew L. Fidler
 ##' @export
-rxWinSetup <- function(rm.rtools=TRUE, rm.python=TRUE){
-    if (!.rxWinRtoolsPath(rm.rtools, rm.python)){
+rxWinSetup <- function(rm.rtools=TRUE){
+    if (!.rxWinRtoolsPath(rm.rtools)){
         message("This package requires Rtools!\nPlease download from http://cran.r-project.org/bin/windows/Rtools/,\ninstall and restart your R session before proceeding.\n");
     }
-    rxWinPythonSetup();
 }
