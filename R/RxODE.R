9f2b3a3ceb5de4f2fcf98fa66c190a891c113e11
Matthew Fidler <matthew.fidler@gmail.com>
2020-03-08 22:16:51 -0500

    Also check for rxode standard model names
    See Issue #178

--- R/RxODE.R
+++ R/RxODE.R
@@ -1362,7 +1362,10 @@ rxCompile.rxModelVars <-  function(model, # Model
     .needCompile <- TRUE
     if (file.exists(.cDllFile)){
         .modVars <- sprintf("%smodel_vars", prefix);
-        if (!missing(prefix) && !missing(dir) && is.loaded(.modVars)){
+        if (!missing(prefix) && !missing(dir) && 
+            regexpr(rex::rex(start,"rx_", n_times(any,32),or("_x64","_i386","_")),
+                    prefix) != -1 &&
+            is.loaded(.modVars)){
             dyn.unload(.cDllFile)
             unlink(.cDllFile)
         } else {
