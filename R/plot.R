cfa940b01dcb764e54f31622bdeab4cb4f51669f
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2020-03-10 15:20:24 -0500

    Plotting fixes

--- R/plot.R
+++ R/plot.R
@@ -34,18 +34,25 @@ rx_theme <- function(){
 ##'@export
 plot.rxSolve <- function(x,y,..., log="") {
     .data <- NULL
+    .call <- as.list(match.call()[-(1:3)])
+    .call <- .call[!names(.call) %in% c("x", "y", "log")]
     .cmts <- c(as.character(substitute(y)),
-               names(sapply(as.character(as.list(match.call()[-(1:3)])),`c`)))
+               names(sapply(as.character(.call),`c`)))
     if (length(.cmts)==1 &&.cmts[1]=="") {
         .cmts <- NULL
     }
     .dat <- rxStack(x,.cmts);
     .nlvl <- 1L
     if (any(names(.dat) == "id")) {
-        .dat$id <- factor(.dat$id)
+        if (any(names(.dat) == "sim.id")){
+            .dat$id <- factor(paste0("id=", .dat$id, ", sim.id=", .dat$sim.id))
+        } else {
+            .dat$id <- factor(.dat$id)
+        }
         .nlvl <- length(levels(.dat$id))
         .dat2 <- .dat[rev(seq_along(.dat$id)), ];
         .dat2$label <- .dat$id
+        .dat2$time <- units::drop_units(.dat2$time)
         row.names(.dat2) <- NULL
         .dat2 <- .dat2[!duplicated(paste0(.dat2$id, .dat2$trt)), ];
         .aes <- aes(.data$time, .data$value, color=.data$id)
@@ -56,6 +63,7 @@ plot.rxSolve <- function(x,y,..., log="") {
         .nlvl <- length(levels(.dat$sim.id))
         .dat2 <- .dat[rev(seq_along(.dat$sim.id)), ];
         .dat2$label <- .dat$id
+        .dat2$time <- units::drop_units(.dat2$time)
         row.names(.dat2) <- NULL
         .dat2 <- .dat2[!duplicated(paste0(.dat2$sim.id, .dat2$trt)), ];
         .aes <- aes(.data$time, .data$value, color=.data$sim.id)
@@ -72,13 +80,15 @@ plot.rxSolve <- function(x,y,..., log="") {
     .repel <- NULL
     .legend <- NULL
     .line <- geom_line(size=1.2)
-    if (.nlvl > 1 && .nlvl < 7 && requireNamespace("ggrepel", quietly = TRUE) &&
-        is.null(.facet)) {
+    .rxSpaghetti <- getOption("RxODE.spaghetti", 7L)
+    .ggrepel <- getOption("RxODE.ggrepel", TRUE) &&
+        requireNamespace("ggrepel", quietly = TRUE)
+    if (.nlvl > 1 && .nlvl < .rxSpaghetti && .ggrepel && is.null(.facet)) {
         .repel <- ggrepel::geom_label_repel(.aesLab, data=.dat2, nudge_x=1,
                                             fontface="bold", size=5)
         .legend <- ggplot2::guides(color = FALSE)
     } else {
-        if (.nlvl < 7) {
+        if (.nlvl < .rxSpaghetti) {
             .legend <- ggplot2::theme(legend.title=ggplot2::element_blank())
         } else {
             .legend <-ggplot2::guides(color = FALSE)
@@ -90,11 +100,11 @@ plot.rxSolve <- function(x,y,..., log="") {
     .logx <- NULL
     .logy <- NULL
     .xgxr <- getOption("RxODE.xgxr", TRUE) &&
-        requireNamespace("xgxr", quietly = TRUE);
+        requireNamespace("xgxr", quietly = TRUE)
     .timex <- NULL
     .unit <- NULL
     .xgxrT <- function(.unit){
-        if (!.xgxr) return(NULL);
+        if (!.xgxr) return(NULL)
         .timex <- xgxr::xgx_scale_x_time_units(.unit)
         if (inherits(.timex, "list")){
             .w <- which(sapply(seq_along(.timex), function(x){
@@ -126,20 +136,22 @@ plot.rxSolve <- function(x,y,..., log="") {
             }
             .timex <- NULL
         } else if (log == "y") {
-            .logy <- xgxr::xgx_scale_y_log10()
-        } else if (log == "xy" || log == "yx") {
-            if (.xgxr) {
-                .logx <- xgxr::xgx_scale_x_log10()
-            } else {
-                .logx <- ggplot2::scale_x_log10()
-            }
-            .dat <- .dat[.dat$time > 0, ]
-            .timex <- NULL
             if (.xgxr) {
                 .logy <- xgxr::xgx_scale_y_log10()
             } else {
-                .logy <- ggplot2::scale_y_log10();
+                .logy <- ggplot2::scale_y_log10()
             }
+        } else if (log == "xy" || log == "yx") {
+            if (.xgxr) {
+                .logx <- xgxr::xgx_scale_x_log10()
+                .logy <- xgxr::xgx_scale_y_log10()
+            } else {
+                .logx <- ggplot2::scale_x_log10()
+                .logy <- ggplot2::scale_y_log10()
+
+            }
+            .dat <- .dat[.dat$time > 0, ]
+            .timex <- NULL
         } else if (log != ""){
             stop(sprintf("'log=\"%s\"' not supported", log))
         }
--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -2,7 +2,7 @@ rxPermissive({
 
     context("plot tests")
     test_that("plot tests", {
-
+        skip_if(utils::packageVersion("ggplot2") <= "3.2.1")
         ## Model from RxODE tutorial
         m1 <-RxODE({
             KA=2.94E-01;
@@ -33,11 +33,20 @@ rxPermissive({
             et(amt=10000, ii=12,until=24) %>%
             et(seq(0, 24, length.out=100))
 
-
-
         s <- rxSolve(m1, ev);
 
-        f <- function(xgxr=FALSE) {
+        ev <- et(timeUnits="hr") %>%
+            et(amt=10000, until = set_units(3, days), ii=12) %>% # loading doses
+            et(seq(0,48,length.out=200)) %>%
+            et(id=1:4)
+
+        set.seed(32)
+        s2 <- rxSolve(m1, ev, params=data.frame(KA=0.294*exp(rnorm(4)), 18.6*exp(rnorm(4))))
+
+        set.seed(32)
+        s20 <- rxSolve(m1, ev, params=data.frame(KA=0.294*exp(rnorm(20)), 18.6*exp(rnorm(20))))
+
+        f <- function(xgxr=FALSE, repel=FALSE) {
             if (xgxr){
                 .xgxtxt <- "xgxr-"
                 options(RxODE.xgxr=TRUE)
@@ -45,6 +54,7 @@ rxPermissive({
                 .xgxtxt <- ""
                 options(RxODE.xgxr=FALSE)
             }
+
             vdiffr::expect_doppelganger(paste0("plot-",.xgxtxt,"C2"), s %>% plot(C2))
             vdiffr::expect_doppelganger(paste0("plot-",.xgxtxt,"C2-log-x"), s %>% plot(C2, log="x"))
             vdiffr::expect_doppelganger(paste0("plot-",.xgxtxt,"C2-log-y"), s %>% plot(C2, log="y"))
@@ -55,12 +65,45 @@ rxPermissive({
             vdiffr::expect_doppelganger(paste0("plot-",.xgxtxt,"all-log-y"), s %>% plot(log="y"))
             vdiffr::expect_doppelganger(paste0("plot-",.xgxtxt,"all-log-xy"), s %>% plot(log="xy"))
             vdiffr::expect_doppelganger(paste0("plot-",.xgxtxt,"all-log-yx"), s %>% plot(log="yx"))
-        }
 
+            ## large
+            vdiffr::expect_doppelganger(paste0("plot-sp-",.xgxtxt,"C2"), s20 %>% plot(C2))
+            vdiffr::expect_doppelganger(paste0("plot-sp-",.xgxtxt,"C2-log-x"), s20 %>% plot(C2, log="x"))
+            vdiffr::expect_doppelganger(paste0("plot-sp-",.xgxtxt,"C2-log-y"), s20 %>% plot(C2, log="y"))
+            vdiffr::expect_doppelganger(paste0("plot-sp-",.xgxtxt,"C2-log-xy"), s20 %>% plot(C2, log="xy"))
+            vdiffr::expect_doppelganger(paste0("plot-sp-",.xgxtxt,"C2-log-yx"), s20 %>% plot(C2, log="yx"))
+            vdiffr::expect_doppelganger(paste0("plot-sp-",.xgxtxt,"all"), s20 %>% plot())
+            vdiffr::expect_doppelganger(paste0("plot-sp-",.xgxtxt,"all-log-x"), s20 %>% plot(log="x"))
+            vdiffr::expect_doppelganger(paste0("plot-sp-",.xgxtxt,"all-log-y"), s20 %>% plot(log="y"))
+            vdiffr::expect_doppelganger(paste0("plot-sp-",.xgxtxt,"all-log-xy"), s20 %>% plot(log="xy"))
+            vdiffr::expect_doppelganger(paste0("plot-sp-",.xgxtxt,"all-log-yx"), s20 %>% plot(log="yx"))
+
+            for (repel in c(TRUE, FALSE)){
+                if (repel) {
+                    .repel <- "repel-"
+                    options(RxODE.ggrepel=TRUE)
+                } else {
+                    .repel <- ""
+                    options(RxODE.ggrepel=FALSE)
+                }
+
+
+                vdiffr::expect_doppelganger(paste0("plot-multi-",.repel,.xgxtxt,"C2"), s2 %>% plot(C2))
+                vdiffr::expect_doppelganger(paste0("plot-multi-",.repel,.xgxtxt,"C2-log-x"), s2 %>% plot(C2, log="x"))
+                vdiffr::expect_doppelganger(paste0("plot-multi-",.repel,.xgxtxt,"C2-log-y"), s2 %>% plot(C2, log="y"))
+                vdiffr::expect_doppelganger(paste0("plot-multi-",.repel,.xgxtxt,"C2-log-xy"), s2 %>% plot(C2, log="xy"))
+                vdiffr::expect_doppelganger(paste0("plot-multi-",.repel,.xgxtxt,"C2-log-yx"), s2 %>% plot(C2, log="yx"))
+                vdiffr::expect_doppelganger(paste0("plot-multi-",.repel,.xgxtxt,"all"), s2 %>% plot())
+                vdiffr::expect_doppelganger(paste0("plot-multi-",.repel,.xgxtxt,"all-log-x"), s2 %>% plot(log="x"))
+                vdiffr::expect_doppelganger(paste0("plot-multi-",.repel,.xgxtxt,"all-log-y"), s2 %>% plot(log="y"))
+                vdiffr::expect_doppelganger(paste0("plot-multi-",.repel,.xgxtxt,"all-log-xy"), s2 %>% plot(log="xy"))
+                vdiffr::expect_doppelganger(paste0("plot-multi-",.repel,.xgxtxt,"all-log-yx"), s2 %>% plot(log="yx"))
+            }
+
+        }
         f(TRUE)
         f(FALSE)
-
-
     })
-
+    options(RxODE.xgxr=TRUE)
+    options(RxODE.ggrepel=TRUE)
 }, silent=TRUE, test="plot")
