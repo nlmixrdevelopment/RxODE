e9b11232e9a06617e46b7b5cee7a45e78b491e7e
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2020-05-06 23:32:54 -0500

    Add the lower-incomplete gamma functions from boost

---
+++ src/boost.cpp
@@ -0,0 +1,34 @@
+#include <boost/math/special_functions/gamma.hpp>
+#include <boost/math/policies/error_handling.hpp>
+
+extern "C" double gamma_p(double a, double z) {
+  return boost::math::gamma_p<double, double>(a, z);
+}
+
+extern "C" double gamma_q(double a, double z) {
+  return boost::math::gamma_q<double, double>(a, z);
+}
+
+extern "C" double tgamma_lower(double a, double z) {
+  return boost::math::tgamma_lower<double, double>(a, z);
+}
+
+extern "C" double gamma_p_derivative(double a, double x) {
+  return boost::math::gamma_p_derivative<double, double>(a, x);
+}
+
+extern "C" double gamma_q_inv(double a, double q) {
+  return boost::math::gamma_q_inv<double, double>(a, q);
+}
+
+extern "C" double gamma_p_inv(double a, double p) {
+  return boost::math::gamma_p_inv<double, double>(a, p);
+}
+
+extern "C" double gamma_q_inva(double a, double q) {
+  return boost::math::gamma_q_inv<double, double>(a, q);
+}
+
+extern "C" double gamma_p_inva(double a, double p) {
+  return boost::math::gamma_p_inv<double, double>(a, p);
+}
