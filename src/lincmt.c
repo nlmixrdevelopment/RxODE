d57039f667ddda30f5a06cca1ad634d8fd5592db
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2020-05-19 16:11:02 -0500

    Move trans and sort to their own separate function

--- src/lincmt.c
+++ src/lincmt.c
@@ -273,19 +273,6 @@ void _update_par_ptr(double t, unsigned int id, rx_solve *rx, int idx){
 
 void doSort(rx_solving_options_ind *ind);
 void calcMtime(int solveid, double *mtime);
-static inline void setLinCmt(rx_solving_options_ind *ind,
-			     int linCmt, double lag, double lag2, double f, double f2,
-			     double rate, double dur, double rate2, double dur2){
-  ind->linCmt=linCmt;
-  ind->lag = lag;
-  ind->lag2 = lag2;
-  ind->f = f;
-  ind->f2 = f2;
-  ind->rate = rate;
-  ind->dur = dur;
-  ind->rate2 = rate2;
-  ind->dur2 = dur2;
-}
 // Advan-style linCmt solutions
 
 
@@ -890,6 +877,221 @@ static inline void doAdvan(double *A,// Amounts
 
 extern int syncIdx(rx_solving_options_ind *ind);
 
+static inline void sortIfNeeded(rx_solve *rx, rx_solving_options_ind *ind, unsigned int id,
+				int *linCmt,
+				double *d_tlag, double *d_tlag2,
+				double *d_F, double *d_F2,
+				double *d_rate1, double *d_dur1,
+				double *d_rate2, double *d_dur2){
+  int sort = 0;
+  //d_tlag, double d_tlag2,
+  /* double d_F, double d_F2, */
+  if (ind->lag != *d_tlag){
+    ind->lag = *d_tlag;
+    sort =1;
+  }
+  if (ind->lag2 != *d_tlag2){
+    ind->lag2 = *d_tlag2;
+    sort = 1;
+  }
+  if (ind->f != *d_F){
+    ind->f = *d_F;
+    sort = 1;
+  }
+  if (ind->f2 != *d_F2){
+    ind->f2 = *d_F2;
+    sort = 1;
+  }
+  if (ind->rate != *d_rate1) {
+    ind->rate = *d_rate1;
+    sort = 1;
+  }
+  if (ind->rate2 != *d_rate2){
+    ind->rate2 = *d_rate2;
+    sort = 1;
+  }
+  if (ind->dur != *d_dur1){
+    ind->dur = *d_dur1;
+    sort = 1;
+  }
+  if (ind->dur2 != *d_dur2){
+    ind->dur2 = *d_dur2;
+    sort = 1;
+  }
+  ind->linCmt = *linCmt;
+  if (sort){
+    rx->needSort = 1;
+    if (rx->nMtime) calcMtime(id, ind->mtime);
+    if (rx->needSort) doSort(ind);
+  }
+}
+
+int parTrans(int *trans, 
+	     double *p1, double *v1,
+	     double *p2, double *p3,
+	     double *p4, double *p5,
+	     unsigned int *ncmt, double *rx_k, double *rx_v, double *rx_k12,
+	     double *rx_k21, double *rx_k13, double *rx_k31){
+  double btemp, ctemp, dtemp;
+  if ((*p5) > 0.){
+    (*ncmt) = 3;
+    switch (*trans){
+    case 1: // cl v q vp
+      (*rx_k) = (*p1)/(*v1); // k = CL/V
+      (*rx_v) = (*v1);
+      (*rx_k12) = (*p2)/(*v1); // k12 = Q/V
+      (*rx_k21) = (*p2)/(*p3); // k21 = Q/Vp
+      (*rx_k13) = (*p4)/(*v1); // k31 = Q2/V
+      (*rx_k31) = (*p4)/(*p5); // k31 = Q2/Vp2
+      break;
+    case 2: // k=(*p1) v=(*v1) k12=(*p2) k21=(*p3) k13=(*p4) k31=(*p5)
+      (*rx_k) = (*p1);
+      (*rx_v) = (*v1);
+      (*rx_k12) = (*p2);
+      (*rx_k21) = (*p3);
+      (*rx_k13) = (*p4);
+      (*rx_k31) = (*p5);
+      break;
+    case 11:
+#undef beta
+#define A (1/(*v1))
+#define B (*p3)
+#define C (*p5)
+#define alpha (*p1)
+#define beta (*p2)
+#define gamma (*p4)
+      (*ncmt)=3;
+      (*rx_v)=1/(A+B+C);
+      btemp = -(alpha*C + alpha*B + gamma*A + gamma*B + beta*A + beta*C)*(*rx_v);
+      ctemp = (alpha*beta*C + alpha*gamma*B + beta*gamma*A)*(*rx_v);
+      dtemp = sqrt(btemp*btemp-4*ctemp);
+      (*rx_k21) = 0.5*(-btemp+dtemp);
+      (*rx_k31) = 0.5*(-btemp-dtemp);
+      (*rx_k)   = alpha*beta*gamma/(*rx_k21)/(*rx_k31);
+      (*rx_k12) = ((beta*gamma + alpha*beta + alpha*gamma) -
+		(*rx_k21)*(alpha+beta+gamma) - (*rx_k) * (*rx_k31) + (*rx_k21)*(*rx_k21))/((*rx_k31) - (*rx_k21));
+      (*rx_k13) = alpha + beta + gamma - ((*rx_k) + (*rx_k12) + (*rx_k21) + (*rx_k31));
+      break;
+    case 10:
+#undef A
+#define A (*v1)
+      (*ncmt)=3;
+      (*rx_v)=1/(A+B+C);
+      btemp = -(alpha*C + alpha*B + gamma*A + gamma*B + beta*A + beta*C)*(*rx_v);
+      ctemp = (alpha*beta*C + alpha*gamma*B + beta*gamma*A)*(*rx_v);
+      dtemp = sqrt(_as_zero(btemp*btemp-4*ctemp));
+      (*rx_k21) = 0.5*(-btemp+dtemp);
+      (*rx_k31) = 0.5*(-btemp-dtemp);
+      (*rx_k)   = alpha*beta*gamma/(*rx_k21)/(*rx_k31);
+      (*rx_k12) = ((beta*gamma + alpha*beta + alpha*gamma) -
+		(*rx_k21)*(alpha+beta+gamma) - (*rx_k) * (*rx_k31) + (*rx_k21)*(*rx_k21))/((*rx_k31) - (*rx_k21));
+      (*rx_k13) = alpha + beta + gamma - ((*rx_k) + (*rx_k12) + (*rx_k21) + (*rx_k31));
+#undef A
+#undef B
+#undef C
+#undef alpha
+#undef beta
+#undef gamma
+#define beta Rf_beta
+      break;
+    default:
+      REprintf(_("invalid trans (3 cmt trans %d)\n"), *trans);
+      return NA_REAL;
+    }
+  } else if ((*p3) > 0.){
+    (*ncmt) = 2;
+    switch (*trans){
+    case 1: // cl=(*p1) v=(*v1) q=(*p2) vp=(*p3)
+      (*rx_k) = (*p1)/(*v1); // k = CL/V
+      (*rx_v) = (*v1);
+      (*rx_k12) = (*p2)/(*v1); // k12 = Q/V
+      (*rx_k21) = (*p2)/(*p3); // k21 = Q/Vp
+      break;
+    case 2: // k=(*p1), (*v1)=v k12=(*p2) k21=(*p3)
+      (*rx_k) = (*p1);
+      (*rx_v) = (*v1);
+      (*rx_k12) = (*p2);
+      (*rx_k21) = (*p3);
+      break;
+    case 3: // cl=(*p1) v=(*v1) q=(*p2) vss=(*p3)
+      (*rx_k) = (*p1)/(*v1); // k = CL/V
+      (*rx_v) = (*v1);
+      (*rx_k12) = (*p2)/(*v1); // k12 = Q/V
+      (*rx_k21) = (*p2)/((*p3)-(*v1)); // k21 = Q/(Vss-V)
+      break;
+    case 4: // alpha=(*p1) beta=(*p2) k21=(*p3)
+      (*rx_v) = (*v1);
+      (*rx_k21) = (*p3);
+      (*rx_k) = (*p1)*(*p2)/(*rx_k21); // (*p1) = alpha (*p2) = beta
+      (*rx_k12) = (*p1) + (*p2) - (*rx_k21) - (*rx_k);
+      break;
+    case 5: // alpha=(*p1) beta=(*p2) aob=(*p3)
+      (*rx_v)=(*v1);
+      (*rx_k21) = ((*p3)*(*p2)+(*p1))/((*p3)+1);
+      (*rx_k) = ((*p1)*(*p2))/(*rx_k21);
+      (*rx_k12) = (*p1)+(*p2) - (*rx_k21) - (*rx_k);
+      break;
+    case 11: // A2 V, alpha=(*p1), beta=(*p2), k21
+#undef beta
+#define A (1/(*v1))
+#define B (*p3)
+#define alpha (*p1)
+#define beta (*p2)
+      (*ncmt)=2;
+      (*rx_v)   = 1/(A+B);
+      (*rx_k21) = (A*beta + B*alpha)*(*rx_v);
+      (*rx_k)   = alpha*beta/(*rx_k21);
+      (*rx_k12) = alpha+beta-(*rx_k21)-(*rx_k);
+      break;
+    case 10: // A=(*v1), alpha=(*p1), beta=(*p2), B=(*p3)
+      // Convert to A (right now A=(*v1) or A=1/(*v1))
+#undef A
+#define A (*v1)
+      (*ncmt)=2;
+      (*rx_v)   = 1/(A + B);
+      (*rx_k21) = (A*beta + B*alpha)*(*rx_v);
+      (*rx_k)   = alpha*beta/(*rx_k21);
+      (*rx_k12) = alpha + beta - (*rx_k21) - (*rx_k);
+      /* REprintf("A: %f, B: %f, alpha: %f, beta: %f\n", A, B, alpha, beta); */
+      /* REprintf("V: %f, k10: %f, k12: %f, k21: %f\n", rx_v, rx_k, rx_k12, rx_k21); */
+#undef A
+#undef B
+#undef alpha
+#undef beta
+#define beta Rf_beta
+      break;
+    default:
+      REprintf(_("invalid trans (2 cmt trans %d)\n"), trans);
+      return NA_REAL;
+    }
+  } else if ((*p1) > 0.){
+    (*ncmt) = 1;
+    switch(*trans){
+    case 1: // cl v
+      (*rx_k) = (*p1)/(*v1); // k = CL/V
+      (*rx_v) = (*v1);
+      break;
+    case 2: // k V
+      (*rx_k) = (*p1);
+      (*rx_v) = (*v1);
+      break;
+    case 11: // alpha V
+      (*rx_k) = (*p1);
+      (*rx_v) = (*v1);
+      break;
+    case 10: // alpha A
+      (*rx_k) = (*p1);
+      (*rx_v) = 1/(*v1);
+      break;
+    default:
+      return 0;
+    }
+  } else {
+    return 0;
+  }
+  return 1;
+}
+
 double linCmtA(rx_solve *rx, unsigned int id, double t, int linCmt,
 	       int i_cmt, int trans, 
 	       double p1, double v1,
@@ -903,6 +1105,8 @@ double linCmtA(rx_solve *rx, unsigned int id, double t, int linCmt,
 	       double d_rate2, double d_dur2) {
   /* REprintf("F: %f; F2: %f\n", d_F, d_F2); */
   rx_solving_options_ind *ind = &(rx->subjects[id]);
+  sortIfNeeded(rx, ind, id, &linCmt, &d_tlag, &d_tlag2, &d_F, &d_F2,
+	       &d_rate1, &d_dur1, &d_rate2, &d_dur2);
   rx_solving_options *op = rx->op;
   int oral0;
   oral0 = (d_ka > 0) ? 1 : 0;
@@ -954,53 +1158,6 @@ double linCmtA(rx_solve *rx, unsigned int id, double t, int linCmt,
       Alast = ind->linCmtAdvan+(op->nlin)*(idx-1);
     }
     setSolved=1;
-    int sort = 0;
-    //d_tlag, double d_tlag2,
-    /* double d_F, double d_F2, */
-    if (ind->lag != d_tlag){
-      ind->lag = d_tlag;
-      sort =1;
-    }
-    if (ind->lag2 != d_tlag2){
-      ind->lag2 = d_tlag2;
-      sort = 1;
-    }
-    if (ind->f != d_F){
-      ind->f = d_F;
-      sort = 1;
-    }
-    if (ind->f2 != d_F2){
-      ind->f2 = d_F2;
-      sort = 1;
-    }
-    if (ind->rate != d_rate1) {
-      ind->rate = d_rate1;
-      sort = 1;
-    }
-    if (ind->rate2 != d_rate2){
-      ind->rate2 = d_rate2;
-      sort = 1;
-    }
-    if (ind->dur != d_dur1){
-      ind->dur = d_dur1;
-      sort = 1;
-    }
-    if (ind->dur2 != d_dur2){
-      ind->dur2 = d_dur2;
-      sort = 1;
-    }
-    ind->linCmt = linCmt;
-    if (sort){
-      rx->needSort = 1;
-      if (rx->nMtime) calcMtime(id, ind->mtime);
-      if (rx->needSort) doSort(ind);
-      return linCmtA(rx, id, t, linCmt, i_cmt, trans, 
-		     p1, v1, p2, p3, p4, p5, d_ka,
-		     d_tlag, d_tlag2, d_F, d_F2,
-		     // Rate and dur can apply to depot and
-		     // Therefore, only 1 model rate is possible with RxODE
-		     d_rate1, d_dur1, d_rate2, d_dur2);
-    }
   } else if (t > it){
     if (!ind->solved[idx])
       linCmtA(rx, id, getTime(ind->ix[idx], ind), linCmt,
@@ -1042,160 +1199,10 @@ double linCmtA(rx_solve *rx, unsigned int id, double t, int linCmt,
   double rx_k21=0;
   double rx_k13=0;
   double rx_k31=0;
-  double btemp, ctemp, dtemp;
-  if (p5 > 0.){
-    ncmt = 3;
-    switch (trans){
-    case 1: // cl v q vp
-      rx_k = p1/v1; // k = CL/V
-      rx_v = v1;
-      rx_k12 = p2/v1; // k12 = Q/V
-      rx_k21 = p2/p3; // k21 = Q/Vp
-      rx_k13 = p4/v1; // k31 = Q2/V
-      rx_k31 = p4/p5; // k31 = Q2/Vp2
-      break;
-    case 2: // k=p1 v=v1 k12=p2 k21=p3 k13=p4 k31=p5
-      rx_k = p1;
-      rx_v = v1;
-      rx_k12 = p2;
-      rx_k21 = p3;
-      rx_k13 = p4;
-      rx_k31 = p5;
-      break;
-    case 11:
-#undef beta
-#define A (1/v1)
-#define B p3
-#define C p5
-#define alpha p1
-#define beta p2
-#define gamma p4
-      ncmt=3;
-      rx_v=1/(A+B+C);
-      btemp = -(alpha*C + alpha*B + gamma*A + gamma*B + beta*A + beta*C)*rx_v;
-      ctemp = (alpha*beta*C + alpha*gamma*B + beta*gamma*A)*rx_v;
-      dtemp = sqrt(btemp*btemp-4*ctemp);
-      rx_k21 = 0.5*(-btemp+dtemp);
-      rx_k31 = 0.5*(-btemp-dtemp);
-      rx_k   = alpha*beta*gamma/rx_k21/rx_k31;
-      rx_k12 = ((beta*gamma + alpha*beta + alpha*gamma) -
-		rx_k21*(alpha+beta+gamma) - rx_k * rx_k31 + rx_k21*rx_k21)/(rx_k31 - rx_k21);
-      rx_k13 = alpha + beta + gamma - (rx_k + rx_k12 + rx_k21 + rx_k31);
-      break;
-    case 10:
-#undef A
-#define A v1
-      ncmt=3;
-      rx_v=1/(A+B+C);
-      btemp = -(alpha*C + alpha*B + gamma*A + gamma*B + beta*A + beta*C)*rx_v;
-      ctemp = (alpha*beta*C + alpha*gamma*B + beta*gamma*A)*rx_v;
-      dtemp = sqrt(_as_zero(btemp*btemp-4*ctemp));
-      rx_k21 = 0.5*(-btemp+dtemp);
-      rx_k31 = 0.5*(-btemp-dtemp);
-      rx_k   = alpha*beta*gamma/rx_k21/rx_k31;
-      rx_k12 = ((beta*gamma + alpha*beta + alpha*gamma) -
-		rx_k21*(alpha+beta+gamma) - rx_k * rx_k31 + rx_k21*rx_k21)/(rx_k31 - rx_k21);
-      rx_k13 = alpha + beta + gamma - (rx_k + rx_k12 + rx_k21 + rx_k31);
-#undef A
-#undef B
-#undef alpha
-#undef beta
-#define beta Rf_beta
-      break;
-    default:
-      REprintf(_("invalid trans (3 cmt trans %d)\n"), trans);
-      return NA_REAL;
-    }
-  } else if (p3 > 0.){
-    ncmt = 2;
-    switch (trans){
-    case 1: // cl=p1 v=v1 q=p2 vp=p3
-      rx_k = p1/v1; // k = CL/V
-      rx_v = v1;
-      rx_k12 = p2/v1; // k12 = Q/V
-      rx_k21 = p2/p3; // k21 = Q/Vp
-      break;
-    case 2: // k=p1, v1=v k12=p2 k21=p3
-      rx_k = p1;
-      rx_v = v1;
-      rx_k12 = p2;
-      rx_k21 = p3;
-      break;
-    case 3: // cl=p1 v=v1 q=p2 vss=p3
-      rx_k = p1/v1; // k = CL/V
-      rx_v = v1;
-      rx_k12 = p2/v1; // k12 = Q/V
-      rx_k21 = p2/(p3-v1); // k21 = Q/(Vss-V)
-      break;
-    case 4: // alpha=p1 beta=p2 k21=p3
-      rx_v = v1;
-      rx_k21 = p3;
-      rx_k = p1*p2/rx_k21; // p1 = alpha p2 = beta
-      rx_k12 = p1 + p2 - rx_k21 - rx_k;
-      break;
-    case 5: // alpha=p1 beta=p2 aob=p3
-      rx_v=v1;
-      rx_k21 = (p3*p2+p1)/(p3+1);
-      rx_k = (p1*p2)/rx_k21;
-      rx_k12 = p1+p2 - rx_k21 - rx_k;
-      break;
-    case 11: // A2 V, alpha=p1, beta=p2, k21
-#undef beta
-#define A (1/v1)
-#define B p3
-#define alpha p1
-#define beta p2
-      ncmt=2;
-      rx_v   = 1/(A+B);
-      rx_k21 = (A*beta + B*alpha)*rx_v;
-      rx_k   = alpha*beta/rx_k21;
-      rx_k12 = alpha+beta-rx_k21-rx_k;
-      break;
-    case 10: // A=v1, alpha=p1, beta=p2, B=p3
-      // Convert to A (right now A=v1 or A=1/v1)
-#undef A
-#define A v1
-      ncmt=2;
-      rx_v   = 1/(A + B);
-      rx_k21 = (A*beta + B*alpha)*rx_v;
-      rx_k   = alpha*beta/rx_k21;
-      rx_k12 = alpha + beta - rx_k21 - rx_k;
-      /* REprintf("A: %f, B: %f, alpha: %f, beta: %f\n", A, B, alpha, beta); */
-      /* REprintf("V: %f, k10: %f, k12: %f, k21: %f\n", rx_v, rx_k, rx_k12, rx_k21); */
-#undef A
-#undef B
-#undef alpha
-#undef beta
-#define beta Rf_beta
-      break;
-    default:
-      REprintf(_("invalid trans (2 cmt trans %d)\n"), trans);
-      return NA_REAL;
-    }
-  } else if (p1 > 0.){
-    ncmt = 1;
-    switch(trans){
-    case 1: // cl v
-      rx_k = p1/v1; // k = CL/V
-      rx_v = v1;
-      break;
-    case 2: // k V
-      rx_k = p1;
-      rx_v = v1;
-      break;
-    case 11: // alpha V
-      rx_k = p1;
-      rx_v = v1;
-      break;
-    case 10: // alpha A
-      rx_k = p1;
-      rx_v = 1/v1;
-      break;
-    default:
-      REprintf(_("invalid trans (1 cmt trans %d)\n"), trans);
-      return NA_REAL;
-    }
-  } else {
+  if (!parTrans(&trans, &p1, &v1, &p2, &p3, &p4, &p5,
+		&ncmt, &rx_k, &rx_v, &rx_k12,
+		&rx_k21, &rx_k13, &rx_k31)){
+    REprintf(_("invalid translation\n"));
     return NA_REAL;
   }
   int evid, wh, cmt, wh100, whI, wh0;
