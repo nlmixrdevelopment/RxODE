ed3576568f19feb216a07bf2d2cfec49f3d14dc8
Matthew Fidler <matthew.fidler@gmail.com>
2020-05-27 14:32:23 -0500

    Add linCmtParse 'c'

--- src/tran.c
+++ src/tran.c
@@ -4417,3 +4417,276 @@ void updateSyntaxCol(){
   p->user.loc.line=lineNum;
   p->user.loc.col=colNum;
 }
+
+////////////////////////////////////////////////////////////////////////////////
+// linCmtParse
+
+typedef struct linCmtStruct {
+  int ka;
+  
+  int k12;
+  int k21;
+  
+  int k13;
+  int k31;
+
+  int c;
+
+  int cl;
+  int cld;
+  
+  int cl1;
+  int cl2;
+  int cl3;
+  int cl4;
+  
+  int cmtc;
+  
+  int clStyle;
+} linCmtStruct;
+
+static inline void linCmtIni(linCmtStruct *lin){
+  lin->ka   = -1;
+  
+  lin->k12  = -1;
+  lin->k21  = -1;
+  
+  lin->k13  = -1;
+  lin->k31  = -1;
+  
+  lin->cmtc = -1;
+
+  lin->cl = -1;
+
+  lin->cld = -1;
+  
+  lin->cl1 = -1;
+  lin->cl2 = -1;
+  lin->cl3 = -1;
+  lin->cl4 = -1;
+
+  lin->c = -1;
+  
+  lin->clStyle=-1;
+}
+
+static inline void linCmtCmt(linCmtStruct *lin, const int cmt){
+  if (lin->cmtc) {
+    lin->cmtc = cmt;
+  }
+  if (lin->cmtc != cmt){
+    error(_("inconsistent central compartment, have both '%d' and '%d'"), lin->cmtc, cmt);
+  }
+}
+
+static inline void linCmtK(linCmtStruct *lin, const char *in, int *index) {
+  if ((in[1] == 'a' || in[1] == 'a') &&
+      in[1] == '\0') {
+    lin->ka = *index;
+    return;
+  }
+  // Support: k12, k21, k13, k31 when central compartment is 1
+  // Also support:  k23, k32, k24, k42 when central compartment is 2
+  if (in[1] == '1') {
+    if (in[2] == '2' && in[3] == '\0') {
+      linCmtCmt(lin, 1);
+      lin->k12 = *index;
+      return;
+    }
+    if (in[2] == '3' && in[3] == '\0') {
+      linCmtCmt(lin, 1);
+      lin->k13 = *index;
+      return;
+    }
+  }
+  if (in[1] == '2') {
+    if (in[2] == '1' && in[3] == '\0') {
+      linCmtCmt(lin, 1);
+      lin->k21 = *index;
+      return;
+    }
+    if (in[2] == '3' && in[3] == '\0') {
+      linCmtCmt(lin, 2);
+      // k23->k12
+      lin->k12 = *index;
+      return;
+    }
+    if (in[2] == '4' && in[3] == '\0') {
+      linCmtCmt(lin, 2);
+      // k24->k13
+      lin->k13 = *index;
+      return;
+    }
+  }
+  if (in[1] == '3')  {
+    if (in[2] == '1' && in[3] == '\0') {
+      linCmtCmt(lin, 1);
+      lin->k31 = *index;
+      return;
+    }
+    if (in[2] == '2' && in[3] == '\0') {
+      linCmtCmt(lin, 2);
+      // k32->k21
+      lin->k21 = *index;
+      return;
+    }
+  }
+  if (in[1] == '4' && in[2] == '2' && in[3] == '\0') {
+    linCmtCmt(lin, 2);
+    // k42 -> k31
+    lin->k31 = *index;
+  }
+}
+
+#define linCmtCl1style 1
+#define linCmtCld1style 2
+
+static inline void linCmtClStr(sbuf *buf, const int style){
+  switch(style){
+  case linCmtCl1style:
+    sAppendN(buf, "Cl#", 3);
+    break;
+  case linCmtCld1style:
+    sAppendN(buf, "Cld#", 4);
+    break;
+  }
+}
+
+static inline void linCmtClStyle(linCmtStruct *lin, const int style) {
+  if (lin->clStyle == -1) {
+    lin->clStyle = style;
+  }
+  if (lin->clStyle != style) {
+    sbuf buf;
+    sIni(&buf);
+    sAppendN(&buf, "cannot mix '", 12);
+    sAppendN(&buf, "' and '", 7);
+    sAppendN(&buf, "' clearance styles", 18);
+    error(buf.s);
+  }
+}
+
+static inline void linCmtC(linCmtStruct *lin, const char *in, int *index) {
+  // CL, CL1, CL2, CL3, CL4
+  // CLD, CLD1, CLD2, CLD3, CL4
+  if (in[1] == '\0'){
+    // C
+    lin->c = *index;
+    return;
+  }
+  if (in[1] == 'l' || in[1] == 'L') {
+    if (in[2] == '\0') {
+      lin->cl = *index;
+      return;
+    }
+    if (in[2] == '1' && in[3] == '\0') {
+      linCmtClStyle(lin, linCmtCl1style);
+      lin->cl1 = *index;
+      return;
+    }
+    if (in[2] == '2' && in[3] == '\0') {
+      linCmtClStyle(lin, linCmtCl1style);
+      lin->cl2 = *index;
+      return;
+    }
+    if (in[2] == '3' && in[3] == '\0') {
+      linCmtClStyle(lin, linCmtCl1style);
+      lin->cl3 = *index;
+      return;
+    }
+    if (in[2] == '4' && in[3] == '\0') {
+      linCmtClStyle(lin, linCmtCl1style);
+      lin->cl4 = *index;
+      return;
+    }
+    if (in[2] == 'd' || in[2] == 'D') {
+      if (in[3] == '\0') {
+	linCmtClStyle(lin, linCmtCld1style);
+	lin->cld = *index;
+	return;
+      }
+      if (in[3] == '1' && in[4] == '\0') {
+	linCmtClStyle(lin, linCmtCld1style);
+	lin->cl1 = *index;
+	return;
+      }
+      if (in[3] == '2' && in[4] == '\0') {
+	linCmtClStyle(lin, linCmtCld1style);
+	lin->cl2 = *index;
+	return;
+      }
+      if (in[3] == '3' && in[4] == '\0') {
+	linCmtClStyle(lin, linCmtCld1style);
+	lin->cl3 = *index;
+	return;
+      }
+      if (in[3] == '4' && in[4] == '\0') {
+	linCmtClStyle(lin, linCmtCld1style);
+	lin->cl4 = *index;
+	return;
+      }
+    }
+  }
+}
+
+static inline void linCmtStr(linCmtStruct *lin, const char *in, int *index) {
+  if (in[0] == 'c' || in[0] == 'C') {
+    linCmtC(lin, in, index);
+    return;
+  }
+  if (in[0] == 'k' || in[0] == 'K') {
+    linCmtK(lin, in, index);
+    return;
+  }
+}
+
+
+SEXP _linCmtParse(SEXP vars){
+  int type = TYPEOF(vars);
+  if (type != STRSXP) {
+    error("can only parse variable names");
+  }
+  linCmtStruct lin;
+  linCmtIni(&lin);
+  for (int i = Rf_length(vars); i--;){
+    linCmtStr(&lin, CHAR(STRING_ELT(vars, i)), &i);
+  }
+  if (lin.cl1 != -1){
+    // Cl1, Cl2, Cl3
+    // -> cl, cl2, cl3
+    if (lin.cl != -1) {
+      error(_("cannot mix 'Cl' and 'Cl1'"));
+    }
+    linCmtCmt(&lin, 1);
+    lin.cl = lin.cl1;
+    lin.cl1 = -1;
+    if (lin.cl4 != -1){
+      error(_("specified clearance for 4th compartment, which does not make sense in this context"));
+    }
+  } else if (lin.cl2 != -1){
+    if (lin.cl != -1){
+      //  Cl2, Cl3, Cl4
+      // -> Cl, cl2, cl3
+      linCmtCmt(&lin, 2);
+      lin.cl = lin.cl2;
+      lin.cl2 = lin.cl3;
+      lin.cl3 = lin.cl4;
+    } else if (lin.cl4 != -1) {
+      // Cl, Cl2, Cl3 keeps the same;  Cl4 doesn't make sense
+      error(_("specified clearance for 4th compartment, which does not make sense in this context"));
+    }
+  } else if (lin.cl != -1){
+    if (lin.cl3 != -1){
+      // Cl, Cl3, Cl4
+      //-> Cl, Cl2, cl3
+      lin.cl2 = lin.cl3;
+      lin.cl3 = lin.cl4;
+      lin.cl4 = -1;
+    }
+  }
+  int trans =-1;
+  if (lin.cl){
+    trans = 1;
+  }
+  return R_NilValue;
+}
