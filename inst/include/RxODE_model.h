5e7de7a532aa3693048b0b89b52a75ac5ab675f2
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2020-08-12 11:38:23 -0500

    Add safeZero option for more items. See #260

--- inst/include/RxODE_model.h
+++ inst/include/RxODE_model.h
@@ -20,8 +20,8 @@
 #define LHS_Rprintf Rprintf
 #define _safe_log(a) (&_solveData->safeZero ? (((a) <= 0) ? log(DOUBLE_EPS) : log(a)) : log(a))
 #define safe_zero(a) (&_solveData->safeZero ? ((a) == 0 ? DOUBLE_EPS : (a)) : (a))
-#define _as_zero(a) (fabs(a) < sqrt(DOUBLE_EPS) ? 0.0 : a)
-#define _as_dbleps(a) (fabs(a) < sqrt(DOUBLE_EPS) ? ((a) < 0 ? -sqrt(DOUBLE_EPS)  : sqrt(DOUBLE_EPS)) : a)
+#define _as_zero(a) (&_solveData->safeZero & fabs(a) < sqrt(DOUBLE_EPS) ? 0.0 : a)
+#define _as_dbleps(a) (&_solveData->safeZero & fabs(a) < sqrt(DOUBLE_EPS) ? ((a) < 0 ? -sqrt(DOUBLE_EPS)  : sqrt(DOUBLE_EPS)) : a)
 #define factorial(a) exp(lgamma1p(a))
 #define sign_exp(sgn, x)(((sgn) > 0.0) ? exp(x) : (((sgn) < 0.0) ? -exp(x) : 0.0))
 #define Rx_pow(a, b) R_pow(a, b)
