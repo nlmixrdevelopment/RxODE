77ff3619d5e38fff14dc3638f4cc677194267eff
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2020-10-10 17:55:58 -0500

    Possible fix for _strdup_printf and tran for CRAN

--- src/strdup_printf.c
+++ src/strdup_printf.c
@@ -2,13 +2,14 @@
 #include <stdarg.h>
 #include <stdlib.h>
 char * _strdup_printf(char * fmt, ...) {
-	va_list va;
-	va_start(va, fmt);
-	int s = vsnprintf(NULL, 0, fmt, va);
-	va_end(va);
-	char * rt = malloc(s);
-	va_start(va, fmt);
-	vsnprintf(rt, s, fmt, va);
-	va_end(va);
-	return rt;
+  char *zero = "";
+  va_list va;
+  va_start(va, fmt);
+  int s = vsnprintf(zero, 0, fmt, va);
+  va_end(va);
+  char * rt = malloc(s);
+  va_start(va, fmt);
+  vsnprintf(rt, s, fmt, va);
+  va_end(va);
+  return rt;
 }
