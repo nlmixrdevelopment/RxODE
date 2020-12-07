e2a1f0d992af30119d4faf43008327f795d3acba
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2020-12-06 20:23:21 -0600

    Revert "Get external pointer for ovar directly; Maybe fix #347"
    This reverts commit ed24eb232e6a998b9abd9d7813dbfe7be16f679f.

--- src/threefry.cpp
+++ src/threefry.cpp
@@ -1377,7 +1377,6 @@ extern "C" void simeps(int id) {
   }
 }
 
-extern "C" int *getOvar();
 
 extern "C" void simeta(int id) {
   rx_solve* rx = getRxSolve_();
@@ -1389,13 +1388,12 @@ extern "C" void simeta(int id) {
     // ind->id  = csub+csim*nsub;
     int csim = floor(ind->id/rx->nsub);
     simvar(&out[0], 1, csim, rx);
-    int *ovar = getOvar();
     double *par_ptr = ind->par_ptr;
     for (int j=0; j < rx->neta; j++){
       // The error pointer is updated if needed
       REprintf("j: %d\n", j );
-      REprintf("ovar[j]: %d\n", ovar[j]);
-      par_ptr[ovar[j]] = out[j];
+      REprintf("ovar[j]: %d\n", op->ovar[j]);
+      par_ptr[op->ovar[j]] = out[j];
       REprintf("out[j]: %d\n", out[j]);
     }
   }
