33575bf136fcbba4725156dad2c6c8e5a19818e5
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2020-12-18 07:19:20 -0600

    Split out to isReservedVariable

--- src/tran.c
+++ src/tran.c
@@ -520,50 +520,60 @@ static inline int assertForbiddenVariables(const char *s) {
   }
   return 1;
 }
+static inline int isReservedVariable(const char *s) {
+  return !strcmp("amt", s) ||
+    !strcmp("time", s) ||
+    !strcmp("podo", s) ||
+    !strcmp("rx__PTR__", s) ||
+    !strcmp("tlast", s) ||
+    // Ignore M_ constants
+    !strcmp("M_E", s) ||
+    !strcmp("M_LOG2E", s) ||
+    !strcmp("M_LOG10E", s) ||
+    !strcmp("M_LN2", s) ||
+    !strcmp("M_LN10", s) ||
+    !strcmp("M_PI", s) ||
+    !strcmp("M_PI_2", s) ||
+    !strcmp("M_PI_4", s) ||
+    !strcmp("M_1_PI", s) ||
+    !strcmp("M_2_PI", s) ||
+    !strcmp("M_2_SQRTPI", s) ||
+    !strcmp("M_SQRT2", s) ||
+    !strcmp("M_SQRT1_2", s) ||
+    !strcmp("M_SQRT_3", s) ||
+    !strcmp("M_SQRT_32", s) ||
+    !strcmp("M_LOG10_2", s) ||
+    !strcmp("M_2PI", s) ||
+    !strcmp("M_SQRT_PI", s) ||
+    !strcmp("M_1_SQRT_2PI", s) ||
+    !strcmp("M_SQRT_2dPI", s) ||
+    !strcmp("M_LN_SQRT_PI", s) ||
+    !strcmp("M_LN_SQRT_2PI", s) ||
+    !strcmp("M_LN_SQRT_PId2", s) ||
+    // newind/t
+    !strcmp("newind", s) ||
+    !strcmp("NEWIND", s) ||
+    !strcmp("t", s);
+}
+
+
+static inline int isKa(const char *s) {
+  if (tb.hasKa) return 1;
+  if (!strcmp("ka", s) || !strcmp("Ka", s) || !strcmp("KA", s) || !strcmp("kA", s)) {
+    tb.hasKa=1;
+    return 1;
+  }
+  return 0;
+}
 
 static inline int skipReservedVariables(const char *s) {
-  if (!strcmp("amt", s)){
+  if (isReservedVariable(s)) {
     tb.ix=-2;
     return 0;
   }
-  if (!strcmp("time", s)) {tb.ix=-2; return 0;}
-  if (!strcmp("podo", s)) {tb.ix=-2; return 0;}
-  if (!strcmp("rx__PTR__", s)) {tb.ix=-2; return 0;}
-  if (!strcmp("tlast", s)) {tb.ix=-2; return 0;}
-  // Ignore M_ constants
-  if (!strcmp("M_E", s)) {tb.ix=-2; return 0;}
-  if (!strcmp("M_LOG2E", s)) {tb.ix=-2; return 0;}
-  if (!strcmp("M_LOG10E", s)) {tb.ix=-2; return 0;}
-  if (!strcmp("M_LN2", s)) {tb.ix=-2; return 0;}
-  if (!strcmp("M_LN10", s)) {tb.ix=-2; return 0;}
-  if (!strcmp("M_PI", s)) {tb.ix=-2; return 0;}
-  if (!strcmp("M_PI_2", s)) {tb.ix=-2; return 0;}
-  if (!strcmp("M_PI_4", s)) {tb.ix=-2; return 0;}
-  if (!strcmp("M_1_PI", s)) {tb.ix=-2; return 0;}
-  if (!strcmp("M_2_PI", s)) {tb.ix=-2; return 0;}
-  if (!strcmp("M_2_SQRTPI", s)) {tb.ix=-2; return 0;}
-  if (!strcmp("M_SQRT2", s)) {tb.ix=-2; return 0;}
-  if (!strcmp("M_SQRT1_2", s)) {tb.ix=-2; return 0;}
-  if (!strcmp("M_SQRT_3", s)) {tb.ix=-2; return 0;}
-  if (!strcmp("M_SQRT_32", s)) {tb.ix=-2; return 0;}
-  if (!strcmp("M_LOG10_2", s)) {tb.ix=-2; return 0;}
-  if (!strcmp("M_2PI", s)) {tb.ix=-2; return 0;}
-  if (!strcmp("M_SQRT_PI", s)) {tb.ix=-2; return 0;}
-  if (!strcmp("M_1_SQRT_2PI", s)) {tb.ix=-2; return 0;}
-  if (!strcmp("M_SQRT_2dPI", s)) {tb.ix=-2; return 0;}
-  if (!strcmp("M_LN_SQRT_PI", s)) {tb.ix=-2; return 0;}
-  if (!strcmp("M_LN_SQRT_2PI", s)) {tb.ix=-2; return 0;}
-  if (!strcmp("M_LN_SQRT_PId2", s)) {tb.ix=-2; return 0;}
   if (!strcmp("pi", s)) tb.isPi=1;
-  if (!strcmp("NA", s)) return 0;
-  if (!strcmp("NaN", s)) return 0;
-  if (!strcmp("Inf", s)) return 0;
-  if (!tb.hasKa && !strcmp("ka", s)) tb.hasKa=1;
-  if (!tb.hasKa && !strcmp("Ka", s)) tb.hasKa=1;
-  if (!tb.hasKa && !strcmp("KA", s)) tb.hasKa=1;
-  if (!tb.hasKa && !strcmp("kA", s)) tb.hasKa=1;
-  if (!strcmp("newind", s)) {tb.ix=-2; return 0;}
-  if (!strcmp("NEWIND", s)) {tb.ix=-2; return 0;}
+  if (!strcmp("NA", s) || !strcmp("NaN", s) || !strcmp("Inf", s)) return 0;
+  isKa(s); // To update tb.hasKa
   return 1;
 }
 
@@ -571,7 +581,6 @@ static inline int skipReservedVariables(const char *s) {
 int new_or_ith(const char *s) {
   int i;
   if (tb.fn) {tb.ix=-2; return 0;}
-  if (!strcmp("t", s)) {tb.ix=-2; return 0;}
   if (!strcmp("lhs", s)){tb.ix=-1; return 0;}
   if (assertForbiddenVariables(s) == 0) return 0;
   if (skipReservedVariables(s) == 0) return 0;
@@ -2199,13 +2208,7 @@ static inline int handleFunctions(nodeInfo ni, char *name, int *i, int *depth, i
     tf->xpn = xpn;
     tf->pn = pn;
     tf->v = (char*)rc_dup_str(xpn->start_loc.s, xpn->end);
-    int ii;
     char *v = tf->v;
-    int isNorm=0, isExp=0, isF=0, isGamma=0, isBeta=0,
-      isPois=0, isT=0, isUnif=0, isWeibull=0, isNormV=0,
-      isLead=0, isFirst=0, isLast=0, isDiff=0, isLinB=0,
-      isPnorm=0, isTad=0, isTafd=0, isTlast = 0, isTfirst = 0,
-      isInd=0;
     if (handleFunctionDosenum(tf) ||
 	handleFunctionTad(tf) ||
 	handleFunctionSum(tf) ||
