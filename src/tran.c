53a7862055a36bc4ea139b07fc4671e7ebf08695
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2020-12-17 18:50:49 -0600

    rgeom() splitout

--- src/tran.c
+++ src/tran.c
@@ -1852,6 +1852,56 @@ static inline int handleFunctionRchisq(transFunctions *tf) {
   return 0;
 }
 
+static inline int handleFunctionRgeom(transFunctions *tf) {
+  if (!strcmp("rxgeom", tf->v) ||
+      !strcmp("rgeom", tf->v) ||
+      (tf->isInd = !strcmp("rigeom", tf->v)) ||
+      (tf->isPois= !strcmp("rxpois", tf->v) ||
+       !strcmp("rpois", tf->v) ||
+       (tf->isInd = !strcmp("ripois", tf->v)))) {
+    if (tb.thread != 0) tb.thread = 2;
+    int ii = d_get_number_of_children(d_get_child(tf->pn,3))+1;
+    if (ii != 1){
+      updateSyntaxCol();
+      if (tf->isPois){
+	updateSyntaxCol();
+	trans_syntax_error_report_fn(_("'ripois'/'rxpois'/'rpois' takes 1 argument 'rxpois(lambda)'"));
+      } else {
+	updateSyntaxCol();
+	trans_syntax_error_report_fn(_("'rigeom'/'rxgeom'/'rgeom' takes 1 argument 'rxgeom(prob)'"));
+      }
+    } else {
+      D_ParseNode *xpn = d_get_child(tf->pn, 2);
+      char *v2 = (char*)rc_dup_str(xpn->start_loc.s, xpn->end);
+      int allSpace=allSpaces(v2);
+      /* Free(v2); */
+      if (allSpace){
+	if (tf->isPois){
+	  updateSyntaxCol();
+	  trans_syntax_error_report_fn(_("'ripois'/'rxpois'/'rpois' takes 1 argument 'rxpois(lambda)'"));
+	} else {
+	  updateSyntaxCol();
+	  trans_syntax_error_report_fn(_("'rigeom'/'rxgeom'/'rgeom' takes 1 argument 'rxgeom(prob)'"));
+	}
+      } else {
+	if (tf->isInd) {
+	  sAppend(&sb, "(double)%s(&_solveData->subjects[_cSub], %d, ", tf->v, tb.nInd);
+	  sAppend(&sbDt, "(double)%s(&_solveData->subjects[_cSub], %d, ", tf->v, tb.nInd++);
+	  foundF0=1;
+	} else {
+	  sAppend(&sb, "(double)%s(&_solveData->subjects[_cSub], ", tf->v);
+	  sAppend(&sbDt, "(double)%s(&_solveData->subjects[_cSub], ", tf->v);
+	}
+	sAppend(&sbt, "%s(", tf->v);
+      }
+    }
+    tf->i[0] = 1;// Parse next arguments
+    tf->depth[0]=1;
+    return 1;
+  }
+  return 0;
+}
+
 static inline int handleFunctions(nodeInfo ni, char *name, int *i, int *depth, int nch, D_ParseNode *xpn, D_ParseNode *pn) {
   if (tb.fn == 1) {
     transFunctions *tf = &_tf;
@@ -1879,53 +1929,8 @@ static inline int handleFunctions(nodeInfo ni, char *name, int *i, int *depth, i
 	handleFunctionPnorm(tf) ||
 	handleFunctionTransit(tf) ||
 	handleFunctionRxnorm(tf) ||
-	handleFunctionRchisq(tf)) {
-      return 1;
-    } else if (!strcmp("rxgeom", v) ||
-	       !strcmp("rgeom", v) ||
-	       (isInd = !strcmp("rigeom", v)) ||
-	       (isPois= !strcmp("rxpois", v) ||
-		!strcmp("rpois", v) ||
-		(isInd = !strcmp("ripois", v)))) {
-      if (tb.thread != 0) tb.thread = 2;
-      ii = d_get_number_of_children(d_get_child(pn,3))+1;
-      if (ii != 1){
-	updateSyntaxCol();
-	if (isPois){
-	  updateSyntaxCol();
-	  trans_syntax_error_report_fn(_("'ripois'/'rxpois'/'rpois' takes 1 argument 'rxpois(lambda)'"));
-	} else {
-	  updateSyntaxCol();
-	  trans_syntax_error_report_fn(_("'rigeom'/'rxgeom'/'rgeom' takes 1 argument 'rxgeom(prob)'"));
-	}
-      } else {
-	D_ParseNode *xpn = d_get_child(pn, 2);
-	char *v2 = (char*)rc_dup_str(xpn->start_loc.s, xpn->end);
-	int allSpace=allSpaces(v2);
-	/* Free(v2); */
-	if (allSpace){
-	  if (isPois){
-	    updateSyntaxCol();
-	    trans_syntax_error_report_fn(_("'ripois'/'rxpois'/'rpois' takes 1 argument 'rxpois(lambda)'"));
-	  } else {
-	    updateSyntaxCol();
-	    trans_syntax_error_report_fn(_("'rigeom'/'rxgeom'/'rgeom' takes 1 argument 'rxgeom(prob)'"));
-	  }
-	} else {
-	  if (isInd) {
-	    sAppend(&sb, "(double)%s(&_solveData->subjects[_cSub], %d, ", v, tb.nInd);
-	    sAppend(&sbDt, "(double)%s(&_solveData->subjects[_cSub], %d, ", v, tb.nInd++);
-	    foundF0=1;
-	  } else {
-	    sAppend(&sb, "(double)%s(&_solveData->subjects[_cSub], ", v);
-	    sAppend(&sbDt, "(double)%s(&_solveData->subjects[_cSub], ", v);
-	  }
-	  sAppend(&sbt, "%s(", v);
-	}
-      }
-      *i = 1;// Parse next arguments
-      *depth=1;
-      /* Free(v); */
+	handleFunctionRchisq(tf) ||
+	handleFunctionRgeom(tf)) {
       return 1;
     } else if (!strcmp("rbinom", v) ||
 	       !strcmp("rxbinom", v) ||
