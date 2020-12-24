27f688e8b7b01c028dbc65664a6f4da264faa3b3
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2020-12-24 00:30:16 -0600

    Split ClVss ClV

--- src/parseLinCmt.c
+++ src/parseLinCmt.c
@@ -5,91 +5,99 @@ int errOff = 0;
 
 int _linCmtParsePro=0;
 
-static inline void linCmtParseTransCl(linCmtStruct *lin, int verbose) {
+static inline void linCmtParseTransClVss(linCmtStruct *lin, int verbose) {
+  lin->ncmt = 2;
+  lin->trans = 3;
+  if (lin->vStyle != -1) {
+    errLin[0] = '\0';
+    errOff=0;
+    snprintf(errLin + errOff, errLinLen-errOff, "cannot mix 'Vss' and '");
+    errOff+=22;
+    linCmtVStr(lin->vStyle);
+    snprintf(errLin + errOff, errLinLen-errOff, "' volumes");
+    errOff+=9;
+    parseFree(0);
+    sFree(&(lin->ret0));
+    sFree(&(lin->ret));
+    Rf_errorcall(R_NilValue, errLin);
+  }
+  if (lin->v == -1) {
+    parseFree(0);
+    sFree(&(lin->ret0));
+    sFree(&(lin->ret));
+    Rf_errorcall(R_NilValue, _("cannot figure out a central volume"));
+  }
+  if (lin->cl2 == -1) {
+    parseFree(0);
+    sFree(&(lin->ret0));
+    sFree(&(lin->ret));
+    Rf_errorcall(R_NilValue, _("cannot figure out distributional clearance"));
+  }
+  sAppend(&(lin->ret0), "%d, %s", lin->trans, lin->mid);
+  sAppend(&(lin->ret0), "%s, ", CHAR(STRING_ELT(lin->vars, lin->cl)));
+  sAppend(&(lin->ret0), "%s, ", CHAR(STRING_ELT(lin->vars, lin->v)));
+  sAppend(&(lin->ret0), "%s, ", CHAR(STRING_ELT(lin->vars, lin->cl2)));
+  sAppend(&(lin->ret0), "%s, 0.0, 0.0, ", CHAR(STRING_ELT(lin->vars, lin->vss)));
+}
+
+static inline void linCmtParseTransClV(linCmtStruct *lin, int verbose) {
+  if (lin->v == -1) {
+    parseFree(0);
+    sFree(&(lin->ret0));
+    sFree(&(lin->ret));
+    Rf_errorcall(R_NilValue, _("cannot figure out a central volume"));
+  }
+  lin->ncmt = 1;
   lin->trans = 1;
-  if (lin->vss != -1) {
+  sAppend(&(lin->ret0), "%d, %s", lin->trans, lin->mid);
+  sAppend(&(lin->ret0), "%s, ", CHAR(STRING_ELT(lin->vars, lin->cl)));
+  sAppend(&(lin->ret0), "%s, ", CHAR(STRING_ELT(lin->vars, lin->v)));
+  if (lin->v2 != -1 || lin->cl2 != -1) {
     lin->ncmt = 2;
-    lin->trans = 3;
-    if (lin->vStyle != -1) {
-      errLin[0] = '\0';
-      errOff=0;
-      snprintf(errLin + errOff, errLinLen-errOff, "cannot mix 'Vss' and '");
-      errOff+=22;
-      linCmtVStr(lin->vStyle);
-      snprintf(errLin + errOff, errLinLen-errOff, "' volumes");
-      errOff+=9;
-      parseFree(0);
-      sFree(&(lin->ret0));
-      sFree(&(lin->ret));
-      Rf_errorcall(R_NilValue, errLin);
-    }
-    if (lin->v == -1) {
-      parseFree(0);
-      sFree(&(lin->ret0));
-      sFree(&(lin->ret));
-      Rf_errorcall(R_NilValue, _("cannot figure out a central volume"));
-    }
     if (lin->cl2 == -1) {
       parseFree(0);
       sFree(&(lin->ret0));
       sFree(&(lin->ret));
       Rf_errorcall(R_NilValue, _("cannot figure out distributional clearance"));
     }
-    sAppend(&(lin->ret0), "%d, %s", lin->trans, lin->mid);
-    sAppend(&(lin->ret0), "%s, ", CHAR(STRING_ELT(lin->vars, lin->cl)));
-    sAppend(&(lin->ret0), "%s, ", CHAR(STRING_ELT(lin->vars, lin->v)));
-    sAppend(&(lin->ret0), "%s, ", CHAR(STRING_ELT(lin->vars, lin->cl2)));
-    sAppend(&(lin->ret0), "%s, 0.0, 0.0, ", CHAR(STRING_ELT(lin->vars, lin->vss)));
-  } else {
-    if (lin->v == -1) {
+    if (lin->v2 == -1) {
       parseFree(0);
       sFree(&(lin->ret0));
       sFree(&(lin->ret));
-      Rf_errorcall(R_NilValue, _("cannot figure out a central volume"));
+      Rf_errorcall(R_NilValue, _("cannot figure out distributional volume"));
     }
-    lin->ncmt = 1;
-    lin->trans = 1;
-    sAppend(&(lin->ret0), "%d, %s", lin->trans, lin->mid);
-    sAppend(&(lin->ret0), "%s, ", CHAR(STRING_ELT(lin->vars, lin->cl)));
-    sAppend(&(lin->ret0), "%s, ", CHAR(STRING_ELT(lin->vars, lin->v)));
-    if (lin->v2 != -1 || lin->cl2 != -1) {
-      lin->ncmt = 2;
-      if (lin->cl2 == -1) {
+    sAppend(&(lin->ret0), "%s, ", CHAR(STRING_ELT(lin->vars, lin->cl2)));
+    sAppend(&(lin->ret0), "%s, ", CHAR(STRING_ELT(lin->vars, lin->v2)));
+    if (lin->v3 != -1 || lin->cl3 != -1) {
+      lin->ncmt = 3;
+      if (lin->cl3 == -1) {
 	parseFree(0);
 	sFree(&(lin->ret0));
 	sFree(&(lin->ret));
-	Rf_errorcall(R_NilValue, _("cannot figure out distributional clearance"));
+	Rf_errorcall(R_NilValue, _("cannot figure out 2nd distributional clearance"));
       }
-      if (lin->v2 == -1) {
+      if (lin->v3 == -1) {
 	parseFree(0);
 	sFree(&(lin->ret0));
 	sFree(&(lin->ret));
-	Rf_errorcall(R_NilValue, _("cannot figure out distributional volume"));
+	Rf_errorcall(R_NilValue, _("cannot figure out 2nd distributional volume"));
       }
-      sAppend(&(lin->ret0), "%s, ", CHAR(STRING_ELT(lin->vars, lin->cl2)));
-      sAppend(&(lin->ret0), "%s, ", CHAR(STRING_ELT(lin->vars, lin->v2)));
-      if (lin->v3 != -1 || lin->cl3 != -1) {
-	lin->ncmt = 3;
-	if (lin->cl3 == -1) {
-	  parseFree(0);
-	  sFree(&(lin->ret0));
-	  sFree(&(lin->ret));
-	  Rf_errorcall(R_NilValue, _("cannot figure out 2nd distributional clearance"));
-	}
-	if (lin->v3 == -1) {
-	  parseFree(0);
-	  sFree(&(lin->ret0));
-	  sFree(&(lin->ret));
-	  Rf_errorcall(R_NilValue, _("cannot figure out 2nd distributional volume"));
-	}
-	sAppend(&(lin->ret0), "%s, ", CHAR(STRING_ELT(lin->vars, lin->cl3)));
-	sAppend(&(lin->ret0), "%s, ", CHAR(STRING_ELT(lin->vars, lin->v3)));
-      } else {
-	sAppendN(&(lin->ret0), "0.0, 0.0, ", 10);
-      }
+      sAppend(&(lin->ret0), "%s, ", CHAR(STRING_ELT(lin->vars, lin->cl3)));
+      sAppend(&(lin->ret0), "%s, ", CHAR(STRING_ELT(lin->vars, lin->v3)));
     } else {
-      sAppendN(&(lin->ret0), "0.0, 0.0, 0.0, 0.0, ", 20);
+      sAppendN(&(lin->ret0), "0.0, 0.0, ", 10);
     }
+  } else {
+    sAppendN(&(lin->ret0), "0.0, 0.0, 0.0, 0.0, ", 20);
+  }
+}
+
+static inline void linCmtParseTransCl(linCmtStruct *lin, int verbose) {
+  lin->trans = 1;
+  if (lin->vss != -1) {
+    linCmtParseTransClVss(lin, verbose);
+  } else {
+    linCmtParseTransClV(lin, verbose);
   }
   if (verbose) RSprintf(_("Detected %d-compartment model in terms of clearance"), lin->ncmt);
 }
