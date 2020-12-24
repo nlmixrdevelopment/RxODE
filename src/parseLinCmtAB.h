dfccc4a297b97b7f678b8e3caec18894a9fec3b7
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2020-12-24 16:24:05 -0600

    Split out lincmtA

--- src/parseLinCmtAB.h
+++ src/parseLinCmtAB.h
@@ -12,23 +12,38 @@ static inline void linCmtB(linCmtStruct *lin, const char *in, int *index) {
   }
 }
 
-static inline void linCmtA(linCmtStruct *lin, const char *in, int *index) {
+static inline int isOnlyA(linCmtStruct *lin, const char *in, int *index) {
   if (in[1] == '\0') {
     lin->a = *index;
-    return;
+    return 1;
   }
+  return 0;
+}
+
+static inline int isOnlyAOB(linCmtStruct *lin, const char *in, int *index){
   if ((in[1] == 'O' || in[1] == 'o') &&
       (in[2] == 'B' || in[2] == 'b') &&
       in[3] == '\0') {
     lin->aob = *index;
-    return;
+    return 1;
   }
+  return 0
+}
+
+static inline int isOnlyAlpha(linCmtStruct *lin, const char *in, int *index) {
   if ((in[1] == 'L' || in[1] == 'l') &&
       (in[2] == 'P' || in[2] == 'p') &&
       (in[3] == 'H' || in[3] == 'h') &&
       (in[4] == 'A' || in[4] == 'a') &&
       in[5] == '\0') {
     lin->alpha = *index;
-    return;
+    return 1;
   }
+  return 0;
+}
+static inline void linCmtA(linCmtStruct *lin, const char *in, int *index) {
+  int tmp = isOnlyA(lin, in, index) ||
+    isOnlyAOB(lin, in, index) ||
+    isOnlyAlpha(lin, in, index);
+  (void)tmp;
 }
