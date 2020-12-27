3d37dc06b0978d9a95cbed9ff9a2d472d5b3dfca
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2020-12-26 21:07:09 -0600

    evid=3 test fixes

--- tests/testthat/test-evid3.R
+++ tests/testthat/test-evid3.R
@@ -3,52 +3,50 @@ rxPermissive({
 
   test_that("evid=3 reset time", {
 
-    mod1 <-RxODE({
-      KA=2.94E-01;
-      CL=1.86E+01;
-      V2=4.02E+01;
-      Q=1.05E+01;
-      V3=2.97E+02;
-      Kin=1;
-      Kout=1;
-      EC50=200;
-      C2 = centr/V2;
-      C3 = peri/V3;
-      d/dt(depot) =-KA*depot;
-      d/dt(centr) = KA*depot - CL*C2 - Q*C2 + Q*C3;
-      d/dt(peri)  =                    Q*C2 - Q*C3;
-      d/dt(eff)  = Kin - Kout*(1-C2/(EC50+C2))*eff;
-      eff(0) = 1
+    mod1 <- RxODE({
+      KA <- 2.94E-01
+      CL <- 1.86E+01
+      V2 <- 4.02E+01
+      Q <- 1.05E+01
+      V3 <- 2.97E+02
+      Kin <- 1
+      Kout <- 1
+      EC50 <- 200
+      C2 <- centr/V2
+      C3 <- peri/V3
+      d/dt(depot) <- -KA*depot
+      d/dt(centr) <- KA*depot - CL*C2 - Q*C2 + Q*C3
+      d/dt(peri) <- Q*C2 - Q*C3
+      d/dt(eff) <- Kin - Kout*(1-C2/(EC50+C2))*eff
+      eff(0) <- 1
       printf("%f\n", time)
     })
 
     et_1 <-
-      #et(amount.units='mg', time.units='hours') %>%
       et(dose=10000, addl=0, ii=0) %>%
-      #et(amt=20000, nbr.doses=5, start.time=120, dosing.interval=24) %>%
       et(0:8)
 
     et_reset <- et(evid=3)
 
     et_2 <-
-      #et(amount.units='mg', time.units='hours') %>%
       et(dose=20000, addl=0, ii=0) %>%
-      #et(amt=20000, nbr.doses=5, start.time=120, dosing.interval=24) %>%
       et(0:8)
 
     et <- dplyr::bind_rows(et_1, et_reset, et_2)
 
-    sink("test-evid3.csv")
-    cat("t\n")
-    x <- rxSolve(mod1, et)
-    sink()
+    t <- tempfile("test-evid3", fileext=".csv")
 
-    d <- read.csv("test-evid3.csv")
-    unlink("test-evid3.csv")
+    .rxWithSink(t, {
+      cat("t\n")
+      x <- rxSolve(mod1, et)
+    })
+
+    d <- read.csv(t)
+    unlink(t)
 
     expect_true(all(d$t < 9))
     expect_true(all(x$time < 9))
-    expect_true(!all(x$C2[x$resetno==1]==x$C2[x$resetno==2]))
+    expect_true(!all(x$C2[x$resetno == 1] == x$C2[x$resetno == 2]))
     expect_true(x$eff[1] == 1)
 
   })
@@ -56,18 +54,18 @@ rxPermissive({
 
   test_that("evid=3 reset time mixed", {
 
-    mod1 <-RxODE({
-      KA=2.94E-01;
-      CL=1.86E+01;
-      V2=4.02E+01;
-      Q=1.05E+01;
-      V3=2.97E+02;
-      Kin=1;
-      Kout=1;
-      EC50=200;
-      C2 = linCmt();
-      d/dt(eff)  = Kin - Kout*(1-C2/(EC50+C2))*eff;
-      eff(0) = 1
+    mod1 <- RxODE({
+      KA <- 2.94E-01
+      CL <- 1.86E+01
+      V2 <- 4.02E+01
+      Q <- 1.05E+01
+      V3 <- 2.97E+02
+      Kin <- 1
+      Kout <- 1
+      EC50 <- 200
+      C2 <- linCmt()
+      d/dt(eff) <- Kin - Kout*(1-C2/(EC50+C2))*eff
+      eff(0) <- 1
       printf("%f\n", time)
     })
 
@@ -88,17 +86,22 @@ rxPermissive({
     et <- dplyr::bind_rows(et_1, et_reset, et_2)
     et
 
-    sink("test-evid3.csv")
-    cat("t\n")
-    x <- rxSolve(mod1, et)
-    sink()
+    t <- tempfile("test-evid3", fileext=".csv")
 
-    d <- read.csv("test-evid3.csv")
-    unlink("test-evid3.csv")
+    .rxWithSink(t, {
+      cat("t\n")
+      x <- rxSolve(mod1, et)
+    })
+
+    d <- read.csv(t)
+    unlink(t)
+
+    d <- read.csv(t)
+    unlink(t)
 
     expect_true(all(d$t < 9))
     expect_true(all(x$time < 9))
-    expect_true(!all(x$C2[x$resetno==1]==x$C2[x$resetno==2]))
+    expect_true(!all(x$C2[x$resetno == 1] == x$C2[x$resetno == 2]))
     expect_true(x$eff[1] == 1)
 
   })
@@ -107,20 +110,20 @@ rxPermissive({
 
   test_that("evid=3 reset time linCmt", {
 
-    mod1 <-RxODE({
-      KA=2.94E-01;
-      CL=1.86E+01;
-      V2=4.02E+01;
-      Q=1.05E+01;
-      V3=2.97E+02;
-      Kin=1;
-      Kout=1;
-      EC50=200;
-      C2 = linCmt();
+    mod1 <- RxODE({
+      KA <- 2.94E-01
+      CL <- 1.86E+01
+      V2 <- 4.02E+01
+      Q <- 1.05E+01
+      V3 <- 2.97E+02
+      Kin <- 1
+      Kout <- 1
+      EC50 <- 200
+      C2 <- linCmt()
       printf("%f\n", time)
     })
 
-    et_1 <-
+    et1 <-
       #et(amount.units='mg', time.units='hours') %>%
       et(dose=10000, addl=0, ii=0) %>%
       #et(amt=20000, nbr.doses=5, start.time=120, dosing.interval=24) %>%
@@ -134,15 +137,15 @@ rxPermissive({
       #et(amt=20000, nbr.doses=5, start.time=120, dosing.interval=24) %>%
       et(0:8)
 
-    et <- dplyr::bind_rows(et_1, et_reset, et_2)
+    et <- dplyr::bind_rows(et1, et_reset, et_2)
 
-    sink("test-evid3.csv")
-    cat("t\n")
-    x <- rxSolve(mod1, et)
-    sink()
+    .rxWithSink(t, {
+      cat("t\n")
+      x <- rxSolve(mod1, et)
+    })
 
-    d <- read.csv("test-evid3.csv")
-    unlink("test-evid3.csv")
+    d <- read.csv(t)
+    unlink(t)
 
     expect_true(all(d$t < 9))
     expect_true(all(x$time < 9))
@@ -153,32 +156,28 @@ rxPermissive({
   test_that("warning for unsorted data with evid=3", {
 
     mod1 <-RxODE({
-      KA=2.94E-01;
-      CL=1.86E+01;
-      V2=4.02E+01;
-      Q=1.05E+01;
-      V3=2.97E+02;
-      Kin=1;
-      Kout=1;
-      EC50=200;
-      C2 = linCmt();
+      KA <- 2.94E-01
+      CL <- 1.86E+01
+      V2 <- 4.02E+01
+      Q <- 1.05E+01
+      V3 <- 2.97E+02
+      Kin <- 1
+      Kout <- 1
+      EC50 <- 200
+      C2 <- linCmt()
     })
 
-    et_1 <-
-      #et(amount.units='mg', time.units='hours') %>%
+    et1 <-
       et(dose=10000, addl=0, ii=0) %>%
-      #et(amt=20000, nbr.doses=5, start.time=120, dosing.interval=24) %>%
       et(0:8)
 
-    et_reset <- et(evid=3)
+    etReset <- et(evid=3)
 
-    et_2 <-
-      #et(amount.units='mg', time.units='hours') %>%
+    et2 <-
       et(dose=20000, addl=0, ii=0) %>%
-      #et(amt=20000, nbr.doses=5, start.time=120, dosing.interval=24) %>%
       et(0:8)
 
-    et <- dplyr::bind_rows(et_1, et_reset, et_2)
+    et <- dplyr::bind_rows(et1, etReset, et2)
 
     et$time[5] <- 9
 
