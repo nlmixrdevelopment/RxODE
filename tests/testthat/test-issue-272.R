9e39fa874d22895146b6ba175c99cfdfee6a161b
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2020-12-26 22:52:09 -0600

    Remove trailing semicolons

--- tests/testthat/test-issue-272.R
+++ tests/testthat/test-issue-272.R
@@ -1,27 +1,27 @@
 rxPermissive({
   test_that("issue 272 and 273", {
     m1 <-RxODE({
-      KA=2.94E-01;
-      CL=1.86E+01;
-      V2=4.02E+01;
-      Q=1.05E+01;
-      V3=2.97E+02;
-      Kin=1;
-      Kout=1;
-      EC50=200;
+      KA=2.94E-01
+      CL=1.86E+01
+      V2=4.02E+01
+      Q=1.05E+01
+      V3=2.97E+02
+      Kin=1
+      Kout=1
+      EC50=200
       ## Added modeled bioavaiblity, duration and rate
-      fdepot = 1;
-      durDepot = 8;
-      rateDepot = 1250;
-      C2 = centr/V2;
-      C3 = peri/V3;
-      d/dt(depot) =-KA*depot;
+      fdepot = 1
+      durDepot = 8
+      rateDepot = 1250
+      C2 = centr/V2
+      C3 = peri/V3
+      d/dt(depot) =-KA*depot
       f(depot) = fdepot
       dur(depot) = durDepot
       rate(depot) = rateDepot
-      d/dt(centr) = KA*depot - CL*C2 - Q*C2 + Q*C3;
-      d/dt(peri)  =                    Q*C2 - Q*C3;
-      d/dt(eff)  = Kin - Kout*(1-C2/(EC50+C2))*eff;
+      d/dt(centr) = KA*depot - CL*C2 - Q*C2 + Q*C3
+      d/dt(peri)  =                    Q*C2 - Q*C3
+      d/dt(eff)  = Kin - Kout*(1-C2/(EC50+C2))*eff
       eff(0) = 1
     })
 
