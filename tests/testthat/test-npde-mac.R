f0eb6ec50477bcf1033d0c1f7cfdeda3d8a0f170
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2021-01-22 08:27:16 -0600

    Use test_path

--- tests/testthat/test-npde-mac.R
+++ tests/testthat/test-npde-mac.R
@@ -2,7 +2,7 @@ rxodeTest({
 
   test_that("npde simulation works on mac nlmixr #460", {
     tempdir <- tempdir()
-    unzip("si.zip", files="si.qs", exdir=tempdir)
+    unzip(test_path("si.zip"), files="si.qs", exdir=tempdir)
     si <- qs::qread(file.path(tempdir, "si.qs"))
     si$object <- RxODE(si$object)
     set.seed(1009)
