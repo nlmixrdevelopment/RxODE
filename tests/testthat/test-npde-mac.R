5fa4e129982590a6c2660fe21f99b80a6205c269
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2021-01-27 08:57:41 -0600

    Be more careful with npde-mac files

--- tests/testthat/test-npde-mac.R
+++ tests/testthat/test-npde-mac.R
@@ -1,9 +1,8 @@
 rxodeTest({
 
   test_that("npde simulation works on mac nlmixr #460", {
-    tempdir <- tempdir()
-    unzip(test_path("si.zip"), files="si.qs", exdir=tempdir)
-    si <- qs::qread(file.path(tempdir, "si.qs"))
+    skip_if(!file.exists(test_path("si.qs")))
+    si <- qs::qread(test_path("si.qs"))
     si$object <- RxODE(si$object)
     set.seed(1009)
     solve <- expect_error(suppressWarnings(do.call(RxODE::rxSolve, si)), NA)
