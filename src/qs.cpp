844ee5f4bd57ecc4799fd098ca57af896f3afdb8
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2021-03-09 10:55:01 -0600

    Bug fix for qs.cpp using imported instead of linked qs

--- src/qs.cpp
+++ src/qs.cpp
@@ -13,12 +13,12 @@ static void loadQs() {
 
 //[[Rcpp::export]]
 Rcpp::CharacterVector rxQs(SEXP const x) {
-  Rcpp::CharacterVector ret(1);
   loadQs();
   Rcpp::Function base91_encode = Rcpp::as<Rcpp::Function>(qsNs["base91_encode"]);
   Rcpp::Function qserialize = Rcpp::as<Rcpp::Function>(qsNs["qserialize"]);
-  ret[0] = base91_encode(qserialize(x, "high", "zstd", 22, 15, false));
-  return ret;
+  return base91_encode(qserialize(x, Rcpp::CharacterVector::create("high"), Rcpp::CharacterVector::create("zstd"),
+				    Rcpp::IntegerVector::create(22),
+				    Rcpp::IntegerVector::create(15), Rcpp::LogicalVector::create(false)));
 }
 
 //[[Rcpp::export]]
@@ -26,5 +26,5 @@ SEXP rxQr(const std::string& encoded_string) {
   loadQs();
   Rcpp::Function base91_decode = Rcpp::as<Rcpp::Function>(qsNs["base91_decode"]);
   Rcpp::Function qdeserialize = Rcpp::as<Rcpp::Function>(qsNs["qdeserialize"]);
-  return qdeserialize(base91_decode(encoded_string), true, false);
+  return qdeserialize(base91_decode(Rcpp::wrap(encoded_string)), true, false);
 }
