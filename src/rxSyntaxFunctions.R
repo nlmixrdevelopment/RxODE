8eba1e6967ad546f1f116655c67d3b56effef376
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2021-03-12 17:07:11 -0600

    Remove last of hasStan; Change vignette to syntax vignette

---
+++ src/rxSyntaxFunctions.R
@@ -0,0 +1,9 @@
+#' A list and description of Rode supported syntax functions
+#'
+#' @format A data frame with 3 columns and 98 or more rows
+#' \describe{
+#' \item{Reserved Name}{Reserved variable Name}
+#' \item{Description}{Description of variable}
+#' \item{Aliases}{Variable Aliases}
+#' }
+"rxSyntaxFunctions"
