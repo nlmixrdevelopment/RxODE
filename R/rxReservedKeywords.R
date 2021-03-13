70f6fea1566aea3243fd492d149881fd51871aaa
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2021-03-13 05:20:18 -0600

    Update documentation for keywords

--- R/rxReservedKeywords.R
+++ R/rxReservedKeywords.R
@@ -1,9 +1,9 @@
-#' A list and description of Rode supported syntax functions
+#' A list and description of Rode supported reserved keywords
 #'
 #' @format A data frame with 3 columns and 98 or more rows
 #' \describe{
-#' \item{Function}{Function Name}
-#' \item{Description}{Function Description}
-#' \item{Aliases}{Function Alaises}
+#' \item{Reserved Name}{Reserved Keyword Name}
+#' \item{Meaning}{Reserved Keyword Meaning}
+#' \item{Alias}{Keyword Alias}
 #' }
 "rxReservedKeywords"
