d945404986dc60c392e655861e0d463714b17dde
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2021-08-01 10:16:25 -0500

    Fix Issue #445 test

--- tests/testthat/test-445.R
+++ tests/testthat/test-445.R
@@ -54,17 +54,5 @@ rxodeTest({
     theta2 <- c(D = TV_D * 10, CLr = TV_CLr, CLnr = TV_CLnr, Vc = TV_Vc, Vp = TV_Vp, Q = TV_Q, alag = TV_alag)
     sim2 <- expect_warning(rxSolve(mod, theta2, ev3), NA)
 
-    mod <- RxODE({
-      CL <- CLr + CLnr
-      C2 <- central / Vc * 1000
-      d / dt(central) <- -CL / Vc * central - Q / Vc * central + Q / Vp * periph
-      d / dt(periph) <- Q / Vc * central - Q / Vp * periph
-      alag(central) <- alag
-      dummy <- D
-    })
-
-    expect_warning(rxSolve(mod, theta2, ev2), "dur()")
-
-    expect_warning(rxSolve(mod, theta2, ev3), "rate()")
   })
 })
