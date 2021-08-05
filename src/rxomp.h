d8527f7ef89dc9fa26be6311c86804d4e2c870d8
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2021-08-05 18:55:58 -0500

    More conservative random number generator w/openMP

--- src/rxomp.h
+++ src/rxomp.h
@@ -1,6 +1,14 @@
 #ifdef _OPENMP
 #include <pthread.h>
 #include <omp.h>
+
+static inline int rx_get_thread(int mx) {
+  int tn = omp_get_thread_num();
+  if (tn < 0) return 0;
+  if (tn <= mx) return tn;
+  return 0;
+}
+
 #else
 
 static inline int omp_get_num_procs(){
@@ -15,7 +23,12 @@ static inline int omp_get_max_threads(){
   return 1;
 }
 
-static inline int omp_get_thread_num(){
+static inline int omp_get_thread_num() {
   return 0;
 }
+
+static inline int rx_get_thread(int mx) {
+  return 0;
+}
+ 
 #endif
