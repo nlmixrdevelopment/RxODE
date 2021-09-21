7349cc12a7efc4b85c115da3aa5ef622f5c6c02f
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2021-09-21 10:24:17 -0500

    start comment tests

---
+++ R/ui.R
@@ -0,0 +1,54 @@
+##' Replace comments with label()
+##'
+##' @param src source function
+##' @return character vector with labels("") replaced in function
+##' @author Matthew Fidler
+##' @noRd
+.rxReplaceCommentWithLabel <- function(src) {
+  .env <- new.env(parent=emptyenv())
+  .env$inIni <- FALSE
+  .env$convertLabel <- FALSE
+  .regIni <- rex::rex(boundary, or("ini", "lotri"), "(", any_spaces, "{")
+  .regOther <- rex::rex(boundary, or(group("}", any_spaces, ")")), group(or("model"), "(", any_spaces, "{"))
+  .regCommentOnBlankLine <- "^ *#+ *(.*) *$"
+  .regLabel <- "^( *[^\n\"]+) *#+ *(.*) *$"
+  .ret <- vapply(src,
+         function(line) {
+           if (regexpr(.regIni, line, perl=TRUE) != -1) {
+             .env$inIni <- TRUE
+           } else if (regexpr(.regOther, line, perl=TRUE) != -1) {
+             .env$inIni <- FALSE
+           } else if (.env$inIni) {
+             if (regexpr(.regCommentOnBlankLine, line) != -1) {
+             } else if (regexpr(.regLabel, line) != -1) {
+               .env$convertLabel <- TRUE
+               .label <- deparse1(sub(.regLabel, "\\2", line))
+               return(sub(.regLabel, paste0("\\1; label(", .label, ")"), line))
+             }
+           }
+           line
+         }, character(1), USE.NAMES = FALSE)
+  if (.env$convertLabel) {
+    cli::cli_alert_info("parameter labels from comments will be replaced by 'label()'")
+  }
+  .ret
+}
+
+##' Convert RxODE/nlmixr model function to a string
+##'
+##' @param fun function name for parsing
+##' @return Modified function
+##' @author Matthew Fidler
+##' @noRd
+.rxFunction2string <- function(fun) {
+  .srcRef <- attr(fun, "srcref")
+  if (is.null(.srcRef)) {
+    cli::cli_alert_info("parameter labels from comments are typically ignored in non-interactive mode")
+    cli::cli_alert_info("Need to run with the source intact to parse comments")
+    .ret <- deparse(fun)
+  } else {
+    .ret <- .rxReplaceCommentWithLabel(as.character(.srcRef, useSource = TRUE))
+  }
+  .ret
+}
+
---
+++ tests/testthat/test-ui.R
@@ -0,0 +1,67 @@
+rxodeTest({
+
+  test_that("comments are parsed correctly", {
+
+    cmt <- c("function() {", "      ini({", "        ## You may label each parameter with a comment",
+             "        tka <- 0.45 # Log Ka", "        tcl <- log(c(0, 2.7, 100)) # Log Cl",
+             "        ## This works with interactive models", "        ## You may also label the preceding line with label(\"label text\")",
+             "        tv <- 3.45; label(\"log V\")", "        ## the label(\"Label name\") works with all models",
+             "        eta.ka ~ 0.6", "        eta.cl ~ 0.3", "        eta.v ~ 0.1",
+             "        add.sd <- 0.7", "      })", "      model({", "        ka <- exp(tka + eta.ka)",
+             "        cl <- exp(tcl + eta.cl)", "        v <- exp(tv + eta.v)",
+             "        linCmt() ~ add(add.sd)", "      })", "    }")
+
+    eq <- c("function() {", "      ini({", "        ## You may label each parameter with a comment",
+            "        tka <- 0.45 ; label(\"Log Ka\")", "        tcl <- log(c(0, 2.7, 100)) ; label(\"Log Cl\")",
+            "        ## This works with interactive models", "        ## You may also label the preceding line with label(\"label text\")",
+            "        tv <- 3.45; label(\"log V\")", "        ## the label(\"Label name\") works with all models",
+            "        eta.ka ~ 0.6", "        eta.cl ~ 0.3", "        eta.v ~ 0.1",
+            "        add.sd <- 0.7", "      })", "      model({", "        ka <- exp(tka + eta.ka)",
+            "        cl <- exp(tcl + eta.cl)", "        v <- exp(tv + eta.v)",
+            "        linCmt() ~ add(add.sd)", "      })", "    }")
+
+    expect_equal(.rxReplaceCommentWithLabel(cmt), eq)
+
+    one.cmt <- function() {
+      ini({
+        ## You may label each parameter with a comment
+        tka <- 0.45 # Log Ka
+        tcl <- log(c(0, 2.7, 100)) # Log Cl
+        ## This works with interactive models
+        ## You may also label the preceding line with label("label text")
+        tv <- 3.45; label("log V")
+        ## the label("Label name") works with all models
+        eta.ka ~ 0.6
+        eta.cl ~ 0.3
+        eta.v ~ 0.1
+        add.sd <- 0.7
+      })
+      model({
+        ka <- exp(tka + eta.ka)
+        cl <- exp(tcl + eta.cl)
+        v <- exp(tv + eta.v)
+        linCmt() ~ add(add.sd)
+      })
+    }
+
+    str <- .rxFunction2string(one.cmt)
+
+    if (!is.null(attr(one.cmt, "srcref"))) {
+      expect_equal(str, eq)
+      attr(one.cmt, "srcref") <- NULL
+    }
+
+    str <- .rxFunction2string(one.cmt)
+
+    expect_equal(str,
+                 c("function () ", "{", "    ini({", "        tka <- 0.45", "        tcl <- log(c(0, 2.7, 100))",
+                   "        tv <- 3.45", "        label(\"log V\")", "        eta.ka ~ 0.6",
+                   "        eta.cl ~ 0.3", "        eta.v ~ 0.1", "        add.sd <- 0.7",
+                   "    })", "    model({", "        ka <- exp(tka + eta.ka)", "        cl <- exp(tcl + eta.cl)",
+                   "        v <- exp(tv + eta.v)", "        linCmt() ~ add(add.sd)",
+                   "    })", "}"))
+
+
+  })
+
+}, test="lvl2")
