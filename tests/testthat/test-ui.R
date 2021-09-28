1036635e24f24d1936d681ac9a18c6129504d9f3
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2021-09-28 14:36:54 -0500

    Update test to include the condition

--- tests/testthat/test-ui.R
+++ tests/testthat/test-ui.R
@@ -61,7 +61,7 @@ rxodeTest({
                    "        tv <- 3.45", "        label(\"log V\")", "        eta.ka ~ 0.6",
                    "        eta.cl ~ 0.3", "        eta.v ~ 0.1", "        add.sd <- 0.7",
                    "    })", "    model({", "        ka <- exp(tka + eta.ka)", "        cl <- exp(tcl + eta.cl)",
-                   "        v <- exp(tv + eta.v)", "        linCmt() ~ add(add.sd)",
+                   "        v <- exp(tv + eta.v)", "        linCmt() ~ add(add.sd) | tmp",
                    "    })", "}"))
 
   })
