a1d5bcf9313f260a1629a50fa07ce9368ed1d091
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2021-11-06 22:12:52 -0500

    Add theta and etas from nlmixr

--- R/piping-model.R
+++ R/piping-model.R
@@ -1,3 +1,15 @@
+.thetamodelVars <- rex::rex(or("tv", "t", "pop", "POP", "Pop", "TV", "T", "cov", "err", "eff"))
+.thetaModelReg <- rex::rex(or(
+  group(start, .thetamodelVars),
+  group(.thetamodelVars, end)))
+
+.etaParts <- c(
+  "eta", "ETA", "Eta", "ppv", "PPV", "Ppv", "iiv", "Iiv", "bsv", "Bsv", "BSV",
+  "bpv", "Bpv", "BPV", "psv", "PSV", "Psv")
+
+.etaModelReg <- rex::rex(or(group(start, or(.etaParts)), group(or(.etaParts), end)))
+
+
 #' @export
 #' @rdname model
 model.function <- function(x, ..., envir=parent.frame()) {
