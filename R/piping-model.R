6bd05970850c05b0ef478d298d037244128f690e
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2021-11-08 14:45:43 -0600

    Add a function to find the original line of the expression

--- R/piping-model.R
+++ R/piping-model.R
@@ -21,4 +21,72 @@ model.function <- function(x, ..., envir=parent.frame()) {
 #' @rdname model
 model.rxUi <- function(x, ..., envir=parent.frame()) {
   .ret <- .copyUi(x) # copy so (as expected) old UI isn't affected by the call
+  .modelLines <- .iniQuoteLines(match.call(expand.dots = TRUE)[-(1:2)])
 }
+
+#' Find the line of the original expression
+#'
+#' @param expr Expression
+#' @param rxui RxODE UI
+#' @param errorLine When TRUE,Should the error line be considered, or consider
+#'   the non-error line
+#' @return The return can be:
+#'
+#'  - Line number of from the original model, if the model matches one
+#'  line uniquely.
+#'
+#'  - Negative number; This indicates the model property (ie
+#'  `f(depot)`) is not found in the model, but the differential
+#'  equation `d/dt(depot)` is defined uniquely i the model and is
+#'  found at the -(line number) returned.
+#'
+#'  - `NA` which means the line (or related line) was not found in the
+#'  model
+#'
+#'  - `NULL` which means a line is duplicated in the model
+#'
+#' @author Matthew L. Fidler
+#'
+#' @noRd
+.getModelLineFromExpression <- function(expr, rxui, errorLine=FALSE) {
+  .origLines <- rxui$lstExpr
+  .errLines <- rxui$predDf$line
+  .ret <- NA_integer_
+  for (.i in seq_along(.origLines)) {
+    .isErrorLine <- .i %in% .errLines
+    if ((errorLine && .isErrorLine) ||
+          (!errorLine && !.isErrorLine)) {
+      .expr <- .origLines[[.i]]
+      if (identical(.expr[[2]], expr)) {
+        if (is.na(.ret)) {
+          .ret <- .i
+        } else {
+          return(NULL)
+        }
+      }
+    }
+  }
+  if (!is.na(.ret)) {
+    return(.ret)
+  }
+  if (!errorLine && length(expr) == 2L) {
+    .expr1 <- expr[[1]]
+    .expr2 <- expr[[2]]
+    if (identical(.expr1, quote(`f`)) ||
+          identical(.expr1, quote(`F`)) ||
+          identical(.expr1, quote(`alag`)) ||
+          identical(.expr1, quote(`lag`)) ||
+          identical(.expr1, quote(`rate`)) ||
+          identical(.expr1, quote(`dur`))) {
+      .expr3 <- eval(parse(text=paste0("quote(d/dt(",as.character(.expr2),"))")))
+      for (.i in seq_along(.origLines)) {
+        .expr <- .origLines[[.i]]
+        if (identical(.expr[[2]], .expr3)) {
+          return(-.i)
+        }
+      }
+    }
+  }
+  return(NA_integer_)
+}
+
