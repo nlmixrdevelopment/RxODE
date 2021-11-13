2acc67dfd3602491057610b9c0450fa2e201d48d
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2021-11-12 18:33:14 -0600

    Break out parts of the large function

--- R/piping-model.R
+++ R/piping-model.R
@@ -24,6 +24,114 @@ model.rxUi <- function(x, ..., envir=parent.frame()) {
   .modelLines <- .quoteCallInfoLines(match.call(expand.dots = TRUE)[-(1:2)])
 }
 
+
+#' This gives a equivalent left handed expression
+#'
+#' @param expr This exchanges expressions ie f() to F()
+#' @return NULL if there isn't an equaivlent expression, or the
+#'   equivalent expression.
+#' @author Matthew L. Fidler
+#' @noRd
+.getModelLineEquivalentLhsExpression <- function(expr) {
+  .expr3 <- NULL
+  if (length(expr) == 2L) {
+    .expr1 <- expr[[1]]
+    .expr2 <- expr[[2]]
+    if (identical(.expr1, quote(`f`))) {
+      .expr3 <- eval(parse(text=paste0("quote(F(",as.character(.expr2),"))")))
+    }
+    if (identical(.expr1, quote(`F`))) {
+      .expr3 <- eval(parse(text=paste0("quote(f(",as.character(.expr2),"))")))
+    }
+    if (identical(.expr1, quote(`lag`))) {
+      .expr3 <- eval(parse(text=paste0("quote(alag(",as.character(.expr2),"))")))
+    }
+    if (identical(.expr1, quote(`alag`))) {
+      .expr3 <- eval(parse(text=paste0("quote(lag(",as.character(.expr2),"))")))
+    }
+  }
+ .expr3
+}
+
+#' Get the model line number from the expression
+#'
+#' @param expr The Left handed expression to find
+#' @param altExpr The alternate expression to find (or `NULL`), this
+#'   is for expressions that are equivalent like `f(center)` and
+#'   `F(center)`.
+#' @param useErrorLine This is a boolean indicating if error lines
+#'   should be considered (`TRUE`).
+#' @param errLines This is an integer vector of the lines where an
+#'   error is defined in the model.
+#' @param origLines This is a list of lines in the `model({})` block
+#'   of the equation.
+#' @return `NULL` for duplicated lines, otherwise the line number (if
+#'   it is found) and `NA` if it is not found.
+#' @author Matthew L. Fidler
+#' @noRd
+.getModelineFromExperssionsAndOriginalLines <- function(expr, altExpr, useErrorLine,
+                                                        errLines, origLines) {
+  .ret <- NA_integer_
+  for (.i in seq_along(origLines)) {
+    .isErrorLine <- .i %in% errLines
+    if ((useErrorLine && .isErrorLine) ||
+          (!useErrorLine && !.isErrorLine)) {
+      .expr <- origLines[[.i]]
+      if (identical(.expr[[2]], expr)) {
+        if (is.na(.ret)) {
+          .ret <- .i
+        } else {
+          return(NULL)
+        }
+      } else if (!is.null(altExpr)) {
+        if (identical(.expr[[2]], altExpr)) {
+          if (is.na(.ret)) {
+            .ret <- .i
+          } else {
+            return(NULL)
+          }
+        }
+      }
+    }
+  }
+  .ret
+}
+#' Get the negative model line for ode based on ode property
+#'
+#' @param expr Left handed expression
+#' @param origLines List of original lines (expressions)
+#' @param errorLine Consider error lines
+#' @return `NA` if the ODE isn't found. Negative number for the lowest
+#'   line of the ODE compartment.
+#' @author Matthew L. Fidler
+#' @noRd
+.getNegativeModelLineForDiffFromProperty <- function(expr, origLines, errorLine) {
+  .ret <- NA_integer_
+  if (!errorLine && length(expr) == 2L) {
+    .expr1 <- expr[[1]]
+    .expr2 <- expr[[2]]
+    if (identical(.expr1, quote(`f`)) ||
+          identical(.expr1, quote(`F`)) ||
+          identical(.expr1, quote(`alag`)) ||
+          identical(.expr1, quote(`lag`)) ||
+          identical(.expr1, quote(`rate`)) ||
+          identical(.expr1, quote(`dur`))) {
+      .expr3 <- eval(parse(text=paste0("quote(d/dt(",as.character(.expr2),"))")))
+      for (.i in seq_along(origLines)) {
+        .expr <- origLines[[.i]]
+        if (identical(.expr[[2]], .expr3)) {
+          if (is.na(.ret)) {
+            .ret <- -.i
+          } else {
+            .ret <- min(-.i, .ret)
+          }
+        }
+      }
+    }
+  }
+  .ret
+}
+
 #' Find the line of the original expression
 #'
 #' @param expr Expression
@@ -51,75 +159,12 @@ model.rxUi <- function(x, ..., envir=parent.frame()) {
 .getModelLineFromExpression <- function(expr, rxui, errorLine=FALSE) {
   .origLines <- rxui$lstExpr
   .errLines <- rxui$predDf$line
-  .ret <- NA_integer_
   .expr0 <- expr
-  .expr3 <- NULL
-  if (length(.expr0) == 2L) {
-    .expr1 <- .expr0[[1]]
-    .expr2 <- .expr0[[2]]
-    if (identical(.expr1, quote(`f`))) {
-      .expr3 <- eval(parse(text=paste0("quote(F(",as.character(.expr2),"))")))
-    }
-    if (identical(.expr1, quote(`F`))) {
-      .expr3 <- eval(parse(text=paste0("quote(f(",as.character(.expr2),"))")))
-    }
-    if (identical(.expr1, quote(`lag`))) {
-      .expr3 <- eval(parse(text=paste0("quote(alag(",as.character(.expr2),"))")))
-    }
-    if (identical(.expr1, quote(`alag`))) {
-      .expr3 <- eval(parse(text=paste0("quote(lag(",as.character(.expr2),"))")))
-    }
-  }
-  for (.i in seq_along(.origLines)) {
-    .isErrorLine <- .i %in% .errLines
-    if ((errorLine && .isErrorLine) ||
-          (!errorLine && !.isErrorLine)) {
-      .expr <- .origLines[[.i]]
-      if (identical(.expr[[2]], expr)) {
-        if (is.na(.ret)) {
-          .ret <- .i
-        } else {
-          return(NULL)
-        }
-      } else if (!is.null(.expr3)) {
-        if (identical(.expr[[2]], .expr3)) {
-          if (is.na(.ret)) {
-            .ret <- .i
-          } else {
-            return(NULL)
-          }
-        }
-      }
-    }
-  }
+  .expr3 <- .getModelLineEquivalentLhsExpression(expr)
+  .ret <- .getModelineFromExperssionsAndOriginalLines(expr, .expr3, errorLine, .errLines, .origLines)
   if (!is.na(.ret)) {
     return(.ret)
   }
-  if (!errorLine && length(expr) == 2L) {
-    .expr1 <- expr[[1]]
-    .expr2 <- expr[[2]]
-    if (identical(.expr1, quote(`f`)) ||
-          identical(.expr1, quote(`F`)) ||
-          identical(.expr1, quote(`alag`)) ||
-          identical(.expr1, quote(`lag`)) ||
-          identical(.expr1, quote(`rate`)) ||
-          identical(.expr1, quote(`dur`))) {
-      .expr3 <- eval(parse(text=paste0("quote(d/dt(",as.character(.expr2),"))")))
-      for (.i in seq_along(.origLines)) {
-        .expr <- .origLines[[.i]]
-        if (identical(.expr[[2]], .expr3)) {
-          if (is.na(.ret)) {
-            .ret <- -.i
-          } else {
-            .ret <- min(-.i, .ret)
-          }
-        }
-      }
-    }
-    if (!is.na(.ret)) {
-      return(.ret)
-    }
-  }
-  return(NA_integer_)
+  .getNegativeModelLineForDiffFromProperty(expr, .origLines, errorLine)
 }
 
